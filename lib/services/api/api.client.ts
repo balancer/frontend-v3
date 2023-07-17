25d6423d16ca812ec8733aabfbcd3ba9f7413a70
Gareth Fuller <gareth.a.f@gmail.com>
2023-07-17 21:38:53 +0100

    chore: Move actions folder

---
+++ lib/services/api/api.client.ts
@@ -0,0 +1,7 @@
+// import { GraphQLClient } from 'graphql-request'
+// import { getSdk } from './generated/types'
+
+// const ApiClient = new GraphQLClient('https://api-v3.balancer.fi/graphql')
+
+// // getSdk creates a typed client that can be used to fetch data
+// export const api = getSdk(ApiClient)
