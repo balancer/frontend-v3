06673856448299a24993c45a12fe98d6982852d7
Gareth Fuller <gareth.a.f@gmail.com>
2023-07-19 21:35:34 +0100

    chore: Add VStack and HStack components

---
+++ components/_base/HStack.tsx
@@ -0,0 +1,54 @@
+import { cn } from '@/lib/utils/styles'
+import { VariantProps, cva } from 'class-variance-authority'
+import { ReactNode } from 'react'
+
+const hStackVariants = cva('flex flex-row', {
+  variants: {
+    align: {
+      start: 'justify-start',
+      center: 'justify-center',
+      end: 'justify-end',
+    },
+    spacing: {
+      none: 'space-x-0',
+      sm: 'space-x-2',
+      md: 'space-x-4',
+      lg: 'space-x-8',
+    },
+    padd: {
+      none: 'p-0',
+      sm: 'p-2',
+      md: 'p-4',
+      lg: 'p-8',
+    },
+  },
+  defaultVariants: {
+    align: 'start',
+    spacing: 'none',
+    padd: 'none',
+  },
+})
+
+interface Props
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof hStackVariants> {
+  children: ReactNode
+}
+
+export function HStack({
+  children,
+  align,
+  spacing,
+  padd,
+  className,
+  ...props
+}: Props) {
+  return (
+    <div
+      className={cn(hStackVariants({ align, spacing, padd, className }))}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
---
+++ components/_base/VStack.tsx
@@ -0,0 +1,54 @@
+import { cn } from '@/lib/utils/styles'
+import { VariantProps, cva } from 'class-variance-authority'
+import { ReactNode } from 'react'
+
+const vStackVariants = cva('flex flex-col', {
+  variants: {
+    align: {
+      start: 'items-start',
+      center: 'items-center',
+      end: 'items-end',
+    },
+    spacing: {
+      none: 'space-y-0',
+      sm: 'space-y-2',
+      md: 'space-y-4',
+      lg: 'space-y-8',
+    },
+    padd: {
+      none: 'p-0',
+      sm: 'p-2',
+      md: 'p-4',
+      lg: 'p-8',
+    },
+  },
+  defaultVariants: {
+    align: 'start',
+    spacing: 'none',
+    padd: 'none',
+  },
+})
+
+interface Props
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof vStackVariants> {
+  children: ReactNode
+}
+
+export function VStack({
+  children,
+  align,
+  spacing,
+  padd,
+  className,
+  ...props
+}: Props) {
+  return (
+    <div
+      className={cn(vStackVariants({ align, spacing, padd, className }))}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
