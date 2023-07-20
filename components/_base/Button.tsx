08568f2e39a98591228c6cf1ddfe486a2a2fd430
Gareth Fuller <gareth.a.f@gmail.com>
2023-07-20 16:52:55 +0100

    chore: Change button variant name

--- components/_base/Button.tsx
+++ components/_base/Button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        flat: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
--- components/_base/Calendar.tsx
+++ components/_base/Calendar.tsx
@@ -40,7 +40,7 @@ function Calendar({
         row: 'flex w-full mt-2',
         cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
-          buttonVariants({ variant: 'ghost' }),
+          buttonVariants({ variant: 'flat' }),
           'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
         ),
         day_selected:
