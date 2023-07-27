038c627a5bffb04515c3c79f429b6d9469b76922
Gareth Fuller <gareth.a.f@gmail.com>
2023-07-27 21:20:07 +0100

    chore: Rename file

--- lib/services/api/apollo.provider.tsx
+++ lib/services/api/apollo.provider.tsx
@@ -4,7 +4,7 @@
 import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support/ssr'
 import { createApolloClient } from '@/lib/services/api/apollo.client'
 // eslint-disable-next-line max-len
-import { ApolloPrimeCacheProvider } from '@/lib/services/api/apollo.prime.cache.provider'
+import { ApolloPrimeCacheProvider } from '@/lib/services/api/apollo-prime-cache.provider'
 
 export function ApolloProviderWrapper({ children }: React.PropsWithChildren) {
   return (
