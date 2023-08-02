1d9df63f7f4bdca384a5e2e454c115c90bbce431
Gareth Fuller <gareth.a.f@gmail.com>
2023-08-02 22:03:41 +0100

    chore: Re-add dark mode toggle

--- components/btns/DarkModeToggle.tsx
+++ components/btns/DarkModeToggle.tsx
@@ -1,5 +1,31 @@
 'use client'
 
+import { MoonIcon, SunIcon } from "@chakra-ui/icons"
+import { Button, useColorMode } from "@chakra-ui/react"
+import { AnimatePresence, motion } from "framer-motion"
+
 export default function DarkModeToggle() {
-  return <span>todo</span>
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  const animationSun = {
+    initial: { rotate: 0, scale: 0, opacity: 0 },
+    animate: { rotate: 90, scale: 1, opacity: 1 },
+    exit: { rotate: 0, scale: 0, opacity: 0 },
+  }
+
+  const animationMoon = {
+    initial: { rotate: 90, scale: 0, opacity: 0 },
+    animate: { rotate: 0, scale: 1, opacity: 1 },
+    exit: { rotate: 90, scale: 0, opacity: 0 }
+  }
+  
+  return (
+    <Button onClick={toggleColorMode}>
+      <AnimatePresence initial={false}>
+        {colorMode === 'light' 
+          ? (<motion.i {...animationSun}><SunIcon /></motion.i>)
+          : (<motion.i {...animationMoon}><MoonIcon /></motion.i>)}
+      </AnimatePresence>
+    </Button>
+  )
 }
