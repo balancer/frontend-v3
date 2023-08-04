8c42cf0984f7d945e0b5b6f4b83310da1bb79f2c
Alberto Gualis <agualis@gmail.com>
2023-08-04 16:09:01 +0200

    Fix lint

--- lib/modules/pools/components/PoolsList.spec.tsx
+++ lib/modules/pools/components/PoolsList.spec.tsx
@@ -2,10 +2,7 @@ import { screen } from '@testing-library/react'
 import PoolsList from './PoolsList'
 import { PoolsList as PoolListType } from '@/lib/modules/pools/types'
 import { renderWithDefaultProviders } from '@/test/utils/render'
-import {
-  defaultPoolListItem1,
-  mockPoolList,
-} from '@/test/msw/mocks/PoolList.handlers'
+import { defaultPoolListItem1, mockPoolList } from '@/test/msw/mocks/PoolList.handlers'
 import { aGqlPoolMinimal } from '@/test/msw/mocks/gqlPoolMinimal.builders'
 
 test('Renders pool details', async () => {
