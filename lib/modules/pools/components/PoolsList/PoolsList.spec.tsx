ee0fdf706aabcf99ad412592847d14959bf19b1d
Alberto Gualis <agualis@gmail.com>
2023-08-17 11:52:19 +0200

    Fix unit tests

--- lib/modules/pools/components/PoolsList/PoolsList.spec.tsx
+++ lib/modules/pools/components/PoolsList/PoolsList.spec.tsx
@@ -34,7 +34,25 @@ describe('PoolList', () => {
     expect(poolName).toBeInTheDocument()
 
     // console.log is only called when there are token balances
-    expect(console.log).toHaveBeenCalledTimes(3)
+    expect(console.log).toHaveBeenCalledTimes(1)
+    expect(console.log).toHaveBeenCalledWith({
+      balances: [
+        {
+          address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+          amount: 0n,
+          chainId: 1,
+          decimals: 8,
+          formatted: '0',
+        },
+        {
+          address: '0xba100000625a3754423978a60c9317c58a424e3d',
+          amount: 0n,
+          chainId: 1,
+          decimals: 8,
+          formatted: '0',
+        },
+      ],
+    })
   })
 
   test('Renders pool details with a custom mocked GQL pool', async () => {
