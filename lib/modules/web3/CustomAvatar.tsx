d093f613834fe85c167d8d73a29ad7bd4abc77ba
Anastasios <anastas3993@gmail.com>
2023-08-28 18:23:13 +0400

    feat: add custom connect btn (#15)
    * feat: add custom connect btn
    
    * chore: Improvements
    
    ---------
    
    Co-authored-by: Gareth Fuller <gareth.a.f@gmail.com>

---
+++ lib/modules/web3/CustomAvatar.tsx
@@ -0,0 +1,17 @@
+import { Image, ImageProps } from '@chakra-ui/react'
+// eslint-disable-next-line max-len
+import { AvatarComponentProps } from '@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/AvatarContext'
+
+export const CustomAvatar = ({
+  address,
+  ensImage,
+  size,
+  alt,
+  ...props
+}: ImageProps & AvatarComponentProps) => {
+  const avatarUrl = ensImage
+    ? ensImage
+    : `https://api.dicebear.com/7.x/personas/svg?seed=${address}`
+
+  return <Image src={avatarUrl} alt={alt} width={size} height={size} {...props} />
+}
