e0450c1e7ca80ba5f974b57f2e287e7fdc3df2db
Gareth Fuller <gareth.a.f@gmail.com>
2023-09-06 19:54:38 +0100

    feat: Add basic search field (#21)
    * chore: Add nice labels for pool type filters
    
    * feat: Update url params as filters change
    
    * chore: Add comments
    
    * refactor: useEffect usage
    
    * chore: Rename PoolList files
    
    * feat: Add basic search field

---
+++ lib/modules/pools/components/PoolList/components/PoolListSearch.tsx
@@ -0,0 +1,48 @@
+import { IconButton, Input, InputGroup, InputRightElement, useBoolean } from '@chakra-ui/react'
+import { debounce } from 'lodash'
+import { useEffect } from 'react'
+import { usePoolList } from '../../../hooks/usePoolList'
+import { HiOutlineX, HiOutlineSearch } from 'react-icons/hi'
+
+export function PoolListSearch() {
+  const [isSearching, { on, off }] = useBoolean()
+  const { refetch, state, searchText, setSearchText } = usePoolList()
+
+  const submitSearch = debounce(async () => {
+    console.log('submitSearch')
+
+    await refetch({ ...state, textSearch: searchText, skip: 0 })
+    off()
+  }, 250)
+
+  useEffect(() => {
+    submitSearch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText])
+
+  return (
+    <InputGroup size="md">
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={searchText}
+        onChange={e => {
+          setSearchText(e.target.value)
+          if (!isSearching) on()
+        }}
+      />
+      <InputRightElement>
+        <IconButton
+          variant="ghost"
+          size="sm"
+          aria-label="Search for a pool"
+          icon={searchText !== '' ? <HiOutlineX /> : <HiOutlineSearch />}
+          isLoading={isSearching}
+          onClick={() => {
+            if (searchText !== '') setSearchText('')
+          }}
+        />
+      </InputRightElement>
+    </InputGroup>
+  )
+}
