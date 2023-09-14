2a23f27957ba790fc25fe2e14830871667b89c97
Daniel <danielBeets@protonmail.com>
2023-09-14 12:25:36 +0200

    reorganize the components to support instant loading state

--- app/pools/[chain]/[balancerVersion]/[id]/layout.tsx
+++ app/pools/[chain]/[balancerVersion]/[id]/layout.tsx
@@ -1,21 +1,12 @@
 'use client'
 import { BalancerVersion } from '@/lib/modules/pool/pool.types'
-import { ChainSlug, slugToChainMap } from '@/lib/modules/pool/pool.utils'
-import { PoolProvider, useSeedPoolCacheQuery } from '@/lib/modules/pool/usePool'
+import { ChainSlug } from '@/lib/modules/pool/pool.utils'
 import { PropsWithChildren } from 'react'
 
 interface Props extends PropsWithChildren {
   params: { id: string; chain: ChainSlug; balancerVersion: BalancerVersion }
 }
 
-export default function PoolLayout({ params: { id, chain, balancerVersion }, children }: Props) {
-  const _chain = slugToChainMap[chain]
-
-  useSeedPoolCacheQuery({ id, chain: _chain, balancerVersion })
-
-  return (
-    <PoolProvider id={id} chain={_chain} balancerVersion={balancerVersion}>
-      {children}
-    </PoolProvider>
-  )
+export default function PoolLayout({ children }: Props) {
+  return <>{children}</>
 }
--- app/pools/[chain]/[balancerVersion]/[id]/page.tsx
+++ app/pools/[chain]/[balancerVersion]/[id]/page.tsx
@@ -1,12 +1,21 @@
 'use client'
-import { usePool } from '@/lib/modules/pool/usePool'
+import { BalancerVersion } from '@/lib/modules/pool/pool.types'
+import { ChainSlug, slugToChainMap } from '@/lib/modules/pool/pool.utils'
+import { PoolProvider, useSeedPoolCacheQuery } from '@/lib/modules/pool/usePool'
+import { PoolDetail } from '@/lib/modules/pool/PoolDetail'
 
-export default function PoolPage() {
-  const { pool } = usePool()
+interface Props {
+  params: { id: string; chain: ChainSlug; balancerVersion: BalancerVersion }
+}
+
+export default function PoolPage({ params: { id, chain, balancerVersion } }: Props) {
+  const _chain = slugToChainMap[chain]
+
+  useSeedPoolCacheQuery({ id, chain: _chain, balancerVersion })
 
   return (
-    <>
-      {pool.name}: {pool.id}
-    </>
+    <PoolProvider id={id} chain={_chain} balancerVersion={balancerVersion}>
+      <PoolDetail />
+    </PoolProvider>
   )
 }
