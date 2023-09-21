3a267bf93bdbbe27999ff8511e85aed802b97537
Ansaf Ahmed <me@ansaf.net>
2023-09-21 19:18:48 +1000

    update: add a common write method

--- lib/contracts/contract.ts
+++ lib/contracts/contract.ts
@@ -1,153 +1,83 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 
-import { PublicClient, WalletClient, useMutation, usePublicClient } from 'wagmi'
+import { Abi, GetFunctionArgs, InferFunctionName } from 'viem'
+import { vaultABI } from '../abi/generated'
+import { useEffect, useState } from 'react'
 import { useNetworkConfig } from '../config/useNetworkConfig'
 import { get } from 'lodash'
-import { useQuery } from '@tanstack/react-query'
-import { Hash, TransactionReceipt } from 'viem'
-import { useToast } from '@chakra-ui/react'
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
 
-export function getContractConfig(contractPath: string, walletClient?: WalletClient) {
+type WriteAbiMutability = 'payable' | 'nonpayable'
+
+const GeneratedABIMap = {
+  'balancer.vault': vaultABI,
+}
+
+export function useContractAddress(contractId: string) {
   const networkConfig = useNetworkConfig()
-  const publicClient = usePublicClient()
-  const address = get(networkConfig.contracts, contractPath)
-  if (walletClient) {
-    return {
-      address,
-      publicClient,
-      walletClient,
+
+  const address = get(networkConfig.contracts, contractId)
+  return address
+}
+
+export function useManagedTransaction<
+  T extends typeof GeneratedABIMap,
+  M extends keyof typeof GeneratedABIMap
+>(
+  contractId: M,
+  functionName: InferFunctionName<T[M], string, WriteAbiMutability>,
+  args?: GetFunctionArgs<T[M], InferFunctionName<T[M], string, WriteAbiMutability>>
+) {
+  const address = useContractAddress(contractId)
+  const [writeArgs, setWriteArgs] = useState(args)
+
+  const prepareQuery = usePrepareContractWrite({
+    abi: GeneratedABIMap[contractId] as Abi,
+    address,
+    functionName: functionName as InferFunctionName<typeof vaultABI, string, WriteAbiMutability>,
+    // This any is 'safe'. The type provided to any is the same type for args that is inferred via the functionName
+    args: writeArgs as any,
+  })
+
+  const writeQuery = useContractWrite(prepareQuery.config)
+  const transactionStatusQuery = useWaitForTransaction({ hash: writeQuery.data?.hash })
+
+  // on successful submission to chain, add tx to cache
+  useEffect(() => {
+    if (writeQuery.data?.hash) {
+      // addTransaction(writeQuery.data?.hash)
     }
+  }, [writeQuery.data?.hash])
+
+  // if parent changes args, update here
+  useEffect(() => {
+    setWriteArgs(args)
+  }, [JSON.stringify(args)])
+
+  const managedWrite = (
+    args?: GetFunctionArgs<T[M], InferFunctionName<T[M], string, WriteAbiMutability>>
+  ) => {
+    if (args) {
+      setWriteArgs(args)
+    }
+    writeQuery.write?.()
+  }
+
+  const managedWriteAsync = async (
+    args?: GetFunctionArgs<T[M], InferFunctionName<T[M], string, WriteAbiMutability>>
+  ) => {
+    if (args) {
+      setWriteArgs(args)
+    }
+    return await writeQuery.writeAsync?.()
   }
 
   return {
-    address,
-    publicClient,
+    txStatus: transactionStatusQuery,
+    simulate: prepareQuery,
+    ...writeQuery,
+    write: managedWrite,
+    writeAsync: managedWriteAsync,
   }
-}
-
-// Create a type that maps method names to argument types
-type ArgumentsType<T> = T extends (...args: infer U) => any ? U : never
-type MutableTuple<T extends readonly any[]> = { -readonly [P in keyof T]: T[P] }
-type AnyFunction = (...args: any[]) => any
-type FunctionMap = Record<string, AnyFunction>
-
-export function useContractQuery<M extends FunctionMap>(contractId: string, contract: { read: M }) {
-  type TReadMethods = keyof typeof contract.read
-
-  function getQuery<K extends TReadMethods>(
-    functionName: K,
-    functionArgs?: MutableTuple<ArgumentsType<(typeof contract.read)[K]>>
-  ) {
-    const callable = contract.read[functionName] as (
-      ...args: any[]
-    ) => ReturnType<(typeof contract.read)[K]>
-    const query = useQuery<ReturnType<(typeof contract.read)[K]>>({
-      queryKey: [contractId, { ...functionArgs }],
-      queryFn: async () => {
-        return callable(...(functionArgs as any[]))
-      },
-    })
-    return query
-  }
-
-  return getQuery
-}
-
-async function confirmTransaction(
-  publicClient: PublicClient,
-  hash: Hash,
-  minConfirmations: number
-) {
-  // 1. retrieve receipt
-  try {
-    const transactionReceipt = await publicClient.getTransactionReceipt({
-      hash,
-    })
-    if (!transactionReceipt) {
-      throw new Error(`Transaction ${hash} was submitted on-chain but no receipt was found.`)
-    }
-
-    // 2. confirm the transaction
-    const isConfirmed = false
-    while (!isConfirmed) {
-      try {
-        const confirmations = await publicClient.getTransactionConfirmations({
-          transactionReceipt,
-        })
-        if (confirmations >= minConfirmations) {
-          break
-        }
-      } catch (confirmationError) {
-        throw new Error(`Failed to confirm ${hash}. ${confirmationError}`)
-      }
-    }
-    // 3. on confirmation, return the receipt
-    return transactionReceipt
-  } catch (receiptError) {
-    throw new Error(`Failed to retrieve transaction receipt for ${hash}. ${receiptError}`)
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function useContractMutation<T, M>(contractId: string, contract: { write: M }) {
-  const networkConfig = useNetworkConfig()
-  const publicClient = usePublicClient()
-  const toast = useToast()
-  type TWriteMethods = keyof typeof contract.write
-
-  function getMutation<K extends TWriteMethods>(
-    functionName: K,
-    functionArgs: MutableTuple<ArgumentsType<(typeof contract.write)[K]>>
-  ) {
-    const callable = contract.write[functionName] as (...args: any[]) => Promise<Hash>
-    const query = useMutation<TransactionReceipt>({
-      mutationKey: [contractId, { ...functionArgs, functionName }],
-      mutationFn: async () => {
-        const hash: Hash = await callable(...(functionArgs as any[]))
-        toast({
-          title: 'Transaction sent',
-          description: 'Transaction sent',
-          status: 'success',
-          duration: 2000,
-          isClosable: true,
-        })
-        const transactionReceipt = await confirmTransaction(
-          publicClient,
-          hash,
-          networkConfig.minConfirmations
-        )
-        toast({
-          title: 'Confirming transaction',
-          description: 'Confirming transaction',
-          status: 'success',
-          duration: 2000,
-          isClosable: true,
-        })
-        return transactionReceipt
-      },
-      onSuccess: () => {
-        toast({
-          title: 'Transaction confirmed',
-          description: 'Transaction confirmed description',
-          status: 'success',
-          duration: 2000,
-          isClosable: true,
-        })
-      },
-      onError: error => {
-        toast({
-          title: 'Transaction error',
-          description: 'Transaction error',
-          status: 'error',
-          duration: 2000,
-          isClosable: true,
-        })
-        console.error(`Transaction failed. ${error}`)
-      },
-    })
-    return query
-  }
-
-  return getMutation
 }
--- lib/contracts/useManagedTransaction.ts
+++
@@ -1,5 +0,0 @@
-
-export default function useManagedWrite(prepareHook: Function, writeHook: Function) {
-
-    
-}
