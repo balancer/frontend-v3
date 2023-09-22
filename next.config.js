0c3f9faf4b0d16a35a322dec87b07cb3e83886cb
groninge <groninge@gmail.com>
2023-09-22 08:45:05 +0200

    setup sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
@@ -8,3 +9,34 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// Injected content via Sentry wizard below
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs')
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: 'balancer-labs',
+    project: 'frontend-v3-test',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+)
