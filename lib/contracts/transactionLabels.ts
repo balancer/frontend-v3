14226f8cbd4f43a69f33443a2cb61b3ce01e27ed
Alberto Gualis <agualis@gmail.com>
2023-09-25 09:31:10 +0200

    add transaction labels example

---
+++ lib/contracts/transactionLabels.ts
@@ -0,0 +1,43 @@
+import { TokenBase } from '../modules/tokens/token.types';
+import { TransactionInfo } from './useGualisWriteContract';
+
+type TransactionLabels = {
+    label: string;
+    loadingLabel: string;
+    confirmingLabel: string;
+    stepTooltip: string;
+  }
+
+
+type BuildTransactionLabels = (args?: any, transactionInfo?: TransactionInfo)=> TransactionLabels
+
+export const buildRelayerApprovalLabels: BuildTransactionLabels = () => {
+  return {
+    label: 'Sign relayer approval',
+    loadingLabel: 'Confirm in wallet',
+    confirmingLabel: 'Signing relayer approval',
+    stepTooltip: 'Your signature is required to use the relayer.'
+  }
+}
+
+enum ApprovalAction {
+  AddLiquidity,
+  Staking,
+  Unapprove,
+  Unwrapping,
+}
+
+interface ApproveTokenParams {
+  token: TokenBase;
+  normalizedAmount: string;
+  spender: string; //Gauge Address
+  actionType: ApprovalAction;
+}
+export const buildTokenApprovalLabels: BuildTransactionLabels = (args: ApproveTokenParams) => {
+  return {
+    label: `Approve ${args.normalizedAmount} for ${args.actionType}`,
+    loadingLabel: 'Confirm approval in wallet',
+    confirmingLabel: 'Confirming',
+    stepTooltip: `You must approve ${args.token.symbol} to ${args.actionType} this token. Approvals are required once per token, per wallet.`
+  }
+}
