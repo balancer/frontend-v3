650cd8de6412907c05a63a8083f40fb79000e326
groninge <groninge@gmail.com>
2023-09-27 08:05:07 +0200

    update hook

--- lib/config/useProjectConfig.ts
+++ lib/config/useProjectConfig.ts
@@ -1,8 +1,14 @@
 import { ProjectConfigBeets } from '@/lib/config/projects/beets'
 import { ProjectConfigBalancer } from '@/lib/config/projects/balancer'
 
-export const PROJECT_CONFIG =
-  process.env.NEXT_PUBLIC_PROJECT_ID === 'beets' ? ProjectConfigBeets : ProjectConfigBalancer
+export const allProjects = {
+  [ProjectConfigBalancer.projectId]: { ...ProjectConfigBalancer },
+  [ProjectConfigBeets.projectId]: { ...ProjectConfigBeets },
+}
+
+export const PROJECT_CONFIG = process.env.NEXT_PUBLIC_PROJECT_ID
+  ? allProjects[process.env.NEXT_PUBLIC_PROJECT_ID]
+  : ProjectConfigBalancer
 
 export function useProjectConfig() {
   return PROJECT_CONFIG
