a11731bc575d59cb0ddef7820fe71674c0191f6c
groninge <groninge@gmail.com>
2023-09-27 08:05:37 +0200

    update metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,19 @@ import { ReactNode } from 'react'
 import { notFound } from 'next/navigation'
 import { createTranslator } from 'next-intl'
 
+// cannot use a hook here
+import { ProjectConfigBeets } from '@/lib/config/projects/beets'
+import { ProjectConfigBalancer } from '@/lib/config/projects/balancer'
+
+const allProjects = {
+  [ProjectConfigBalancer.projectId]: { ...ProjectConfigBalancer },
+  [ProjectConfigBeets.projectId]: { ...ProjectConfigBeets },
+}
+
+const PROJECT_CONFIG = process.env.NEXT_PUBLIC_PROJECT_ID
+  ? allProjects[process.env.NEXT_PUBLIC_PROJECT_ID]
+  : ProjectConfigBalancer
+
 export function generateStaticParams() {
   return [{ locale: 'en' }]
 }
@@ -11,10 +24,16 @@ export function generateStaticParams() {
 export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
   const messages = (await import(`../../messages/${locale}.json`)).default
   const t = createTranslator({ locale, messages })
+  const { name, projectId } = PROJECT_CONFIG
+  const iconUrl = `/images/icons/${projectId}.ico`
 
   return {
-    title: t('RootLayout.title'),
+    title: t('RootLayout.title', { name }),
     description: t('RootLayout.description'),
+    icons: {
+      icon: [{ url: iconUrl }],
+      shortcut: [iconUrl],
+    },
   }
 }
 
