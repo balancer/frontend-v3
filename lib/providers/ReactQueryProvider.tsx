e759c9a7f4acecfc1de5129217c3a50e03e735d4
Alberto Gualis <agualis@gmail.com>
2023-09-29 12:45:12 +0200

    Remove old react query provider

--- lib/providers/ReactQueryProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React, { PropsWithChildren, useState } from 'react'
-
-export default function ReactQueryProvider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient())
-
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-}
