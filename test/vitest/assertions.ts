7f7900dc2b38b4c2706f7a011d1ba6d14f0a45a5
Alberto Gualis <agualis@gmail.com>
2023-10-09 17:59:11 +0200

    Rename helper function

--- test/vitest/assertions.ts
+++ test/vitest/assertions.ts
@@ -3,6 +3,6 @@ import { Mock } from 'vitest'
 /**
  * Returns the parameters used in the first call to the provided mock
  */
-export function firstCallParams<TArgs extends any[] = any>(mock: Mock<TArgs>): TArgs[0] {
+export function firstMockCallParams<TArgs extends any[] = any>(mock: Mock<TArgs>): TArgs[0] {
   return mock.mock.calls[0][0]
 }
