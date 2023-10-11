d901724a0649808ac2390a3cb8de2cb64ea35480
Alberto Gualis <agualis@gmail.com>
2023-10-11 14:39:59 +0200

    Use contract type

--- app/debug/TransactionState.tsx
+++ app/debug/TransactionState.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useWaitForTransaction } from 'wagmi'
+import { TransactionResult } from '@/lib/contracts/contract.types'
 
 // Example component to illustrate how to render transaction info given a tx hash
-export function TransactionState({ result }: { result: ReturnType<typeof useWaitForTransaction> }) {
+export function TransactionState({ result }: { result: TransactionResult }) {
   const { data } = result
   return <div>Gas used: {data?.gasUsed.toString()}</div>
 }
