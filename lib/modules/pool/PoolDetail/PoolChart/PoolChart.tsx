a94ee1ab9925e44884242242a40bd226f957582a
alter-eggo <anastas3993@gmail.com>
2023-10-18 17:54:28 +0400

    fix: move charts logic to hook

--- lib/modules/pool/PoolDetail/PoolChart/PoolChart.tsx
+++ lib/modules/pool/PoolDetail/PoolChart/PoolChart.tsx
@@ -1,105 +1,27 @@
 'use client'
-import { usePool } from '@/lib/modules/pool/usePool'
+
 import { Box, HStack, Stack, Text } from '@chakra-ui/react'
-
 import ReactECharts from 'echarts-for-react'
-import { useCallback, useMemo, useState } from 'react'
-
-import { format } from 'date-fns'
 import { PoolChartTypeTabs } from './PoolChartTypeTabs'
-import {
-  PoolChartTab,
-  poolChartTabs,
-  poolChartTypeOptions,
-  defaultPoolChartOptions,
-  usePoolSnapshots,
-} from './usePoolCharts'
-import { GqlPoolSnapshotDataRange } from '@/lib/services/api/generated/graphql'
+import { usePoolCharts } from './usePoolCharts'
 import { PoolChartPeriodSelector } from './PoolChartPeriodSelector'
 import Loading from '@/app/pools/[chain]/[variant]/[id]/loading'
 import numeral from 'numeral'
 
 export function PoolChart() {
-  const { pool } = usePool()
-
-  const [activeTab, setActiveTab] = useState(poolChartTabs[0].value)
-  const [chartValue, setChatValue] = useState(0)
-  const [chartDate, setChartDate] = useState('')
-  const [activePeriod, setActivePeriod] = useState(GqlPoolSnapshotDataRange.ThirtyDays)
-
-  const { data, loading } = usePoolSnapshots(pool?.id, activePeriod)
-
-  const chartData = useMemo(() => {
-    const snapshots = data?.snapshots
-    if (!snapshots) return
-
-    if (activeTab === PoolChartTab.TVL) {
-      return snapshots.map(snapshot => {
-        return [snapshot.timestamp, snapshot.totalLiquidity]
-      })
-    }
-
-    if (activeTab === PoolChartTab.FEES) {
-      return snapshots.map(snapshot => {
-        return [snapshot.timestamp, snapshot.fees24h]
-      })
-    }
-
-    return snapshots.map(snapshot => {
-      return [snapshot.timestamp, snapshot.volume24h]
-    })
-  }, [data?.snapshots, activeTab])
-
-  const options = useMemo(() => {
-    const activeTabOptions = poolChartTypeOptions[activeTab]
-
-    return {
-      ...defaultPoolChartOptions,
-      series: [
-        {
-          type: activeTabOptions.type,
-          data: chartData,
-          smooth: true,
-          symbol: 'none',
-          lineStyle: {
-            width: 2,
-          },
-          itemStyle: {
-            color: activeTabOptions.color,
-            borderRadius: 100,
-          },
-          emphasis: {
-            itemStyle: {
-              color: activeTabOptions.hoverColor,
-              borderColor: activeTabOptions.hoverBorderColor,
-            },
-          },
-          areaStyle: activeTabOptions.areaStyle,
-          animationEasing: function (k: number) {
-            return k === 1 ? 1 : 1 - Math.pow(2, -10 * k)
-          },
-        },
-      ],
-    }
-  }, [chartData, activeTab])
-
-  const handleAxisMoved = useCallback(
-    ({ dataIndex }: { dataIndex: number }) => {
-      const chartHoverValue = chartData?.[dataIndex]?.[1]
-      const chartHoverDate = chartData?.[dataIndex]?.[0]
-      if (!chartHoverValue || !chartHoverDate) return
-
-      setChatValue(Number(chartHoverValue))
-      setChartDate(format(new Date(Number(chartHoverDate) * 1000), 'dd MMM yyyy'))
-    },
-    [chartData]
-  )
-
-  const handleMouseLeave = useCallback(() => {
-    const lastChartData = chartData?.[chartData.length - 1]
-    setChatValue(Number(lastChartData?.[1]))
-    setChartDate(format(new Date(Number(lastChartData?.[0]) * 1000), 'dd MMM yyyy'))
-  }, [chartData])
+  const {
+    activeTab,
+    setActiveTab,
+    activePeriod,
+    setActivePeriod,
+    chartValue,
+    chartDate,
+    loading,
+    chartData,
+    options,
+    handleAxisMoved,
+    handleMouseLeave,
+  } = usePoolCharts()
 
   return (
     <Stack
--- lib/modules/pool/PoolDetail/PoolChart/usePoolCharts.tsx
+++ lib/modules/pool/PoolDetail/PoolChart/usePoolCharts.tsx
@@ -7,6 +7,8 @@ import {
   GqlPoolSnapshotDataRange,
 } from '@/lib/services/api/generated/graphql'
 import { useQuery } from '@apollo/experimental-nextjs-app-support/ssr'
+import { useCallback, useMemo, useState } from 'react'
+import { usePool } from '../../usePool'
 
 export enum PoolChartTab {
   VOLUME = 'volume',
@@ -174,4 +176,101 @@ export function usePoolSnapshots(
     },
     notifyOnNetworkStatusChange: true,
   })
+}
+
+export function usePoolCharts() {
+  const { pool } = usePool()
+
+  const [activeTab, setActiveTab] = useState(poolChartTabs[0].value)
+  const [chartValue, setChatValue] = useState(0)
+  const [chartDate, setChartDate] = useState('')
+  const [activePeriod, setActivePeriod] = useState(GqlPoolSnapshotDataRange.ThirtyDays)
+
+  const { data, loading } = usePoolSnapshots(pool?.id, activePeriod)
+
+  const chartData = useMemo(() => {
+    const snapshots = data?.snapshots
+    if (!snapshots) return
+
+    if (activeTab === PoolChartTab.TVL) {
+      return snapshots.map(snapshot => {
+        return [snapshot.timestamp, snapshot.totalLiquidity]
+      })
+    }
+
+    if (activeTab === PoolChartTab.FEES) {
+      return snapshots.map(snapshot => {
+        return [snapshot.timestamp, snapshot.fees24h]
+      })
+    }
+
+    return snapshots.map(snapshot => {
+      return [snapshot.timestamp, snapshot.volume24h]
+    })
+  }, [data?.snapshots, activeTab])
+
+  const options = useMemo(() => {
+    const activeTabOptions = poolChartTypeOptions[activeTab]
+
+    return {
+      ...defaultPoolChartOptions,
+      series: [
+        {
+          type: activeTabOptions.type,
+          data: chartData,
+          smooth: true,
+          symbol: 'none',
+          lineStyle: {
+            width: 2,
+          },
+          itemStyle: {
+            color: activeTabOptions.color,
+            borderRadius: 100,
+          },
+          emphasis: {
+            itemStyle: {
+              color: activeTabOptions.hoverColor,
+              borderColor: activeTabOptions.hoverBorderColor,
+            },
+          },
+          areaStyle: activeTabOptions.areaStyle,
+          animationEasing: function (k: number) {
+            return k === 1 ? 1 : 1 - Math.pow(2, -10 * k)
+          },
+        },
+      ],
+    }
+  }, [chartData, activeTab])
+
+  const handleAxisMoved = useCallback(
+    ({ dataIndex }: { dataIndex: number }) => {
+      const chartHoverValue = chartData?.[dataIndex]?.[1]
+      const chartHoverDate = chartData?.[dataIndex]?.[0]
+      if (!chartHoverValue || !chartHoverDate) return
+
+      setChatValue(Number(chartHoverValue))
+      setChartDate(format(new Date(Number(chartHoverDate) * 1000), 'dd MMM yyyy'))
+    },
+    [chartData]
+  )
+
+  const handleMouseLeave = useCallback(() => {
+    const lastChartData = chartData?.[chartData.length - 1]
+    setChatValue(Number(lastChartData?.[1]))
+    setChartDate(format(new Date(Number(lastChartData?.[0]) * 1000), 'dd MMM yyyy'))
+  }, [chartData])
+
+  return {
+    loading,
+    activeTab,
+    setActiveTab,
+    activePeriod,
+    setActivePeriod,
+    chartValue,
+    chartDate,
+    options,
+    handleAxisMoved,
+    handleMouseLeave,
+    chartData,
+  }
 }
