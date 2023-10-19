0b699995f7262681396eb4bd952880725f12c9ea
groninge <groninge@gmail.com>
2023-10-19 14:45:19 +0200

    add the grid view

--- lib/modules/pool/PoolList/PoolList.tsx
+++ lib/modules/pool/PoolList/PoolList.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { PoolListTable } from './components/PoolListTable/PoolListTable'
-import { VStack } from '@chakra-ui/react'
+import { HStack, VStack } from '@chakra-ui/react'
 import { PoolListFilters } from './components/PoolListFilters'
 import { useAccount } from 'wagmi'
 import { useTokens } from '@/lib/modules/tokens/useTokens'
 import { useTokenBalances } from '@/lib/modules/tokens/useTokenBalances'
+import { PoolListGrid } from './components/PoolListGrid/PoolListGrid'
+import { PoolListViewType, ViewType } from './components/PoolListViewType'
+import React from 'react'
 
 export function PoolList() {
   const { address } = useAccount()
@@ -13,10 +16,16 @@ export function PoolList() {
 
   useTokenBalances(address, tokens)
 
+  const [viewType, setViewType] = React.useState<ViewType>('list')
+
   return (
     <VStack align="start" spacing="md">
-      <PoolListFilters />
-      <PoolListTable />
+      <HStack justifyContent="space-between" w="full">
+        <PoolListFilters />
+        <PoolListViewType setViewType={setViewType} />
+      </HStack>
+      {viewType === 'list' && <PoolListTable />}
+      {viewType === 'grid' && <PoolListGrid />}
     </VStack>
   )
 }
