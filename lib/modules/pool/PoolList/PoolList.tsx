18c2ea8c71e7873f2799cd90fd9d201d6c0b078e
groninge <groninge@gmail.com>
2023-10-23 08:17:27 +0200

    add & use sorting select

--- lib/modules/pool/PoolList/PoolList.tsx
+++ lib/modules/pool/PoolList/PoolList.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { PoolListTable } from './components/PoolListTable/PoolListTable'
-import { HStack, VStack } from '@chakra-ui/react'
+import { HStack, Stack, VStack } from '@chakra-ui/react'
 import { PoolListFilters } from './components/PoolListFilters'
 import { useAccount } from 'wagmi'
 import { useTokens } from '@/lib/modules/tokens/useTokens'
@@ -10,20 +10,26 @@ import { PoolListGrid } from './components/PoolListGrid/PoolListGrid'
 import { PoolListViewType } from './components/PoolListViewType'
 import { usePoolListViewType } from './components/usePoolListViewType'
 import React from 'react'
+import { PoolListSortType } from './components/PoolListSortType'
+import { useBreakpoints } from '@/lib/hooks/useBreakpoints'
 
 export function PoolList() {
   const { address } = useAccount()
   const { tokens } = useTokens()
   const { viewType } = usePoolListViewType()
+  const { isMobile } = useBreakpoints()
 
   useTokenBalances(address, tokens)
 
   return (
     <VStack align="start" spacing="md">
-      <HStack justifyContent="space-between" w="full">
+      <Stack direction={['column-reverse', 'row']} w="full" alignItems="flex-start">
         <PoolListFilters />
-        <PoolListViewType />
-      </HStack>
+        <HStack justifyContent={isMobile ? 'space-between' : 'flex-end'} w="full">
+          {(viewType === 'grid' || isMobile) && <PoolListSortType />}
+          <PoolListViewType />
+        </HStack>
+      </Stack>
       {viewType === 'list' && <PoolListTable />}
       {viewType === 'grid' && <PoolListGrid />}
     </VStack>
