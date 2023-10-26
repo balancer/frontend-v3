db71d8c0381456d8066d7b4880571868c4a07ab4
Alberto Gualis <agualis@gmail.com>
2023-10-26 12:44:36 +0200

    Fix useBalance

--- app/(app)/debug2/JoinPool2.tsx
+++ app/(app)/debug2/JoinPool2.tsx
@@ -4,7 +4,9 @@ import TransactionFlow from '@/components/btns/transaction-steps/TransactionFlow
 import { useConstructJoinPoolStep } from '@/lib/modules/steps/join/useConstructJoinPoolStep'
 import { useUserAccount } from '@/lib/modules/web3/useUserAccount'
 import { Flex, VStack } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { useBalance } from 'wagmi'
+import { FetchBalanceResult } from 'wagmi/dist/actions'
 
 export function JoinPool2() {
   const poolId = '0x68e3266c9c8bbd44ad9dca5afbfe629022aee9fe000200000000000000000512' // Balancer 50COMP-50wstETH
@@ -13,7 +15,12 @@ export function JoinPool2() {
   const { address } = useUserAccount()
   const wstETHAddress = '0x198d7387fa97a73f05b8578cdeff8f2a1f34cd1f'
 
-  const { data: wstETHBalance } = useBalance({ address, token: wstETHAddress })
+  const [wstETHBalance, setWstETHBalance] = useState<FetchBalanceResult | null>(null)
+  const { data } = useBalance({ address, token: wstETHAddress })
+
+  useEffect(() => {
+    if (data) setWstETHBalance(data)
+  }, [data])
 
   function handleJoinCompleted() {
     console.log('Join completed')
@@ -30,7 +37,8 @@ export function JoinPool2() {
         />
         {/* <Button onClick={() => joinQuery.refetch()}>Refetch</Button> */}
       </Flex>
-      <Flex>wsETH User Balance: {wstETHBalance ? wstETHBalance.formatted : '-'} </Flex>
+
+      <Flex>wsETH User Balance: {wstETHBalance ? wstETHBalance.formatted : '-'}</Flex>
     </VStack>
   )
 }
