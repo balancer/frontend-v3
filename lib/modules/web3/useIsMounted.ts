cae61a65957544af7f1357e363d255c76f656d1d
Alberto Gualis <agualis@gmail.com>
2023-10-30 12:18:49 +0100

    add useUserTokenBalance hook to use useIsMounted

--- lib/modules/web3/useIsMounted.ts
+++ lib/modules/web3/useIsMounted.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 
+// The usage of mounted helps to overcome nextjs hydration mismatch
+// errors where the state of the user on the server pass is different
+// than the state on the client side rehydration.
 export function useIsMounted() {
   const [mounted, setMounted] = useState(false)
   useEffect(() => {
