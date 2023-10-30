930c2ea0b14dd1d5ac93b00232b52853a747745c
Gareth Fuller <gareth.a.f@gmail.com>
2023-10-30 16:45:35 +0000

    chore: Update chakra theme (#59)
    * chore: Update theme with tokens
    
    * chore: Undo border color change

--- lib/services/chakra/theme.ts
+++ lib/services/chakra/theme.ts
@@ -68,6 +68,20 @@ export const balTheme: ThemeOverride = {
       lg: '1.5rem',
       xl: '2rem',
     },
+    sizes: {
+      maxContent: '1320px',
+    },
+    radii: {
+      default: 'md',
+      xs: '0.125rem',
+      sm: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+      full: '9999px',
+    },
   },
 }
 
