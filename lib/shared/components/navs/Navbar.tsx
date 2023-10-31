f0494a677364ad1b4e7bdaa0b7ebd72164af570f
uiuxxx <uxuidefi@gmail.com>
2023-10-31 16:37:12 +0000

    Updated the nav

--- lib/shared/components/navs/Navbar.tsx
+++ lib/shared/components/navs/Navbar.tsx
@@ -1,17 +1,26 @@
 'use client'
 import Link from 'next/link'
 import DarkModeToggle from '../btns/DarkModeToggle'
-import { HStack } from '@chakra-ui/react'
+import { Box, Flex, HStack } from '@chakra-ui/react'
 import { ConnectWallet } from '@/lib/modules/web3/ConnectWallet'
 
 export function Navbar() {
   return (
-    <HStack padding="md">
-      <Link href="/">Home</Link>
-      <Link href="/pools">Pools</Link>
-      <Link href="/debug">Debug</Link>
-      <DarkModeToggle />
-      <ConnectWallet />
-    </HStack>
+    <Flex
+      flexDirection={{ base: 'column', md: 'row' }}
+      justify={{ base: 'flex-start', md: 'space-between' }}
+    >
+      <HStack padding="md" order={{ md: '2' }}>
+        <DarkModeToggle />
+        <ConnectWallet />
+      </HStack>
+      <HStack padding="md" spacing="lg">
+        <Link href="/">
+          <Box>&#9775; Balancer</Box>
+        </Link>
+        <Link href="/pools">Pools</Link>
+        <Link href="/debug">Debug</Link>
+      </HStack>
+    </Flex>
   )
 }
