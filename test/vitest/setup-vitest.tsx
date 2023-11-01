d9fba966c13319e5e5fa403f9f139b2f37d9b832
Alberto Gualis <agualis@gmail.com>
2023-11-01 12:19:47 +0100

    Use cross-fetch in test environment

--- test/vitest/setup-vitest.tsx
+++ test/vitest/setup-vitest.tsx
@@ -2,6 +2,15 @@ import '@testing-library/jest-dom'
 import { apolloTestClient } from '../utils/apollo-test-client'
 import { configure } from '@testing-library/react'
 import '@/lib/shared/utils/bigint'
+import fetch from "cross-fetch";
+
+/*
+  Using the default node-fetch in node 18 causes a viem exception in integration tests
+  (Expected signal to be an instanceof AbortSignal)
+  Replacing fetch with cross-fetch implementation solves the issue
+*/
+global.fetch = fetch
+
 
 if (process.env.VITE_USE_PRODUCTION_WAGMI == 'true') {
   console.log('🚨  Running tests with production wagmi config  🚨')
