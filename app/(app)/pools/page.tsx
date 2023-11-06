7caaa4216586f87e0ff80033a3aff4124f7ff639
Gareth Fuller <gareth.a.f@gmail.com>
2023-11-06 16:46:05 +0000

    chore: Remove log

--- app/(app)/pools/page.tsx
+++ app/(app)/pools/page.tsx
@@ -5,18 +5,11 @@ import {
 } from '@/lib/modules/pool/PoolList/usePoolList'
 import { PoolList } from '@/lib/modules/pool/PoolList/PoolList'
 import { Box } from '@chakra-ui/react'
-import { useUserData } from '@/lib/modules/user/useUserData'
-import { useEffect } from 'react'
 
 export const dynamic = 'force-dynamic'
 
 export default function Home() {
   usePoolListSeedCacheQuery()
-  const { balances } = useUserData()
-
-  useEffect(() => {
-    console.log('balances', balances)
-  }, [balances])
 
   return (
     <PoolListProvider>
