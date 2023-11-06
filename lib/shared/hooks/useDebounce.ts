4c3f224c4e48bf6250383fd3ea19b0575d3f6530
Alberto Gualis <agualis@gmail.com>
2023-11-06 14:26:16 +0100

    Simplify TS optional

--- lib/shared/hooks/useDebounce.ts
+++ lib/shared/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { debounce } from 'lodash'
 import { useMemo } from 'react'
 
-export function useDebounce(changeHandler: (...args: any) => any, debounceMilliseconds?: 300) {
+export function useDebounce(changeHandler: (...args: any) => any, debounceMilliseconds = 300) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedChangeHandler = useMemo(() => debounce(changeHandler, debounceMilliseconds), [])
   return debouncedChangeHandler
