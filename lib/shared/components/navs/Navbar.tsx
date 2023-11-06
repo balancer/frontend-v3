21b3aa911bbccd00d49a46da828e6da0335b5689
Daniel <danielBeets@protonmail.com>
2023-11-06 19:08:50 +0800

    add prefetch

--- lib/shared/components/navs/Navbar.tsx
+++ lib/shared/components/navs/Navbar.tsx
@@ -7,9 +7,15 @@ import { ConnectWallet } from '@/lib/modules/web3/ConnectWallet'
 export function Navbar() {
   return (
     <HStack padding="md">
-      <Link href="/">Home</Link>
-      <Link href="/pools">Pools</Link>
-      <Link href="/debug">Debug</Link>
+      <Link href="/" prefetch={true}>
+        Home
+      </Link>
+      <Link href="/pools" prefetch={true}>
+        Pools
+      </Link>
+      <Link href="/debug" prefetch={true}>
+        Debug
+      </Link>
       <DarkModeToggle />
       <ConnectWallet />
     </HStack>
