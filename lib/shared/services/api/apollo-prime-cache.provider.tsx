53fc9d8cfc17525f1f1e896ec3e572b327d8731b
Daniel <danielBeets@protonmail.com>
2023-11-07 16:18:50 +0800

    use seed apollo cache strat

--- lib/shared/services/api/apollo-prime-cache.provider.tsx
+++ lib/shared/services/api/apollo-prime-cache.provider.tsx
@@ -10,25 +10,17 @@ import {
   GetAppGlobalDataDocument,
   GetAppGlobalDataQuery,
 } from '@/lib/shared/services/api/generated/graphql'
-import { useRef } from 'react'
-import { useApolloClient } from '@apollo/client'
+import { useSeedApolloCache } from '@/lib/shared/hooks/useSeedApolloCache'
 
 interface Props extends React.PropsWithChildren {
   data: GetAppGlobalDataQuery
 }
 
 export function ApolloPrimeCacheProvider({ children, data }: Props) {
-  const loaded = useRef(false)
-  const client = useApolloClient()
-
-  if (!loaded.current) {
-    client.writeQuery({
-      query: GetAppGlobalDataDocument,
-      data: data,
-    })
-
-    loaded.current = true
-  }
+  useSeedApolloCache({
+    query: GetAppGlobalDataDocument,
+    data: data,
+  })
 
   return <>{children}</>
 }
