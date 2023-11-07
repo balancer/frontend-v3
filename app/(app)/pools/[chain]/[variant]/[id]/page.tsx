3619ac9adb047aaf32b93adc7285c7ed7896e298
Daniel <danielBeets@protonmail.com>
2023-11-07 09:36:00 +0800

    use rsc approach on pool detail

--- app/(app)/pools/[chain]/[variant]/[id]/page.tsx
+++ app/(app)/pools/[chain]/[variant]/[id]/page.tsx
@@ -1,27 +1,32 @@
-'use client'
 import { FetchPoolProps } from '@/lib/modules/pool/pool.types'
 import { ChainSlug, slugToChainMap } from '@/lib/modules/pool/pool.utils'
-import { PoolProvider, useSeedPoolCacheQuery } from '@/lib/modules/pool/usePool'
+import { PoolProvider } from '@/lib/modules/pool/usePool'
 import { PoolDetail } from '@/lib/modules/pool/PoolDetail/PoolDetail'
 import { Box } from '@chakra-ui/react'
+import { GetPoolDocument } from '@/lib/shared/services/api/generated/graphql'
+import { getApolloServerClient } from '@/lib/shared/services/api/apollo-server.client'
+import { getNetworkConfig } from '@/lib/config/app.config'
 
 interface Props {
   params: Omit<FetchPoolProps, 'chain'> & { chain: ChainSlug }
 }
 
-export default function PoolPage({ params: { id, chain, variant } }: Props) {
+export default async function PoolPage({ params: { id, chain, variant } }: Props) {
   const _chain = slugToChainMap[chain]
+  const { chainId } = getNetworkConfig(_chain)
 
-  const { data } = useSeedPoolCacheQuery({ id, chain: _chain, variant })
-
-  console.log('pool id', data.pool.id)
+  const { data } = await getApolloServerClient().query({
+    query: GetPoolDocument,
+    variables: { id },
+    context: { headers: { ChainId: chainId } },
+  })
 
   if (!data.pool) {
     return <Box>Pool does not exist ({id})</Box>
   }
 
   return (
-    <PoolProvider id={id} chain={_chain} variant={variant}>
+    <PoolProvider id={id} chain={_chain} variant={variant} initialQuery={data}>
       <PoolDetail />
     </PoolProvider>
   )
--- lib/modules/pool/usePool.tsx
+++ lib/modules/pool/usePool.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
-import { GetPoolDocument } from '@/lib/shared/services/api/generated/graphql'
-import { createContext, PropsWithChildren } from 'react'
+import { GetPoolDocument, GetPoolQuery } from '@/lib/shared/services/api/generated/graphql'
+import { createContext, PropsWithChildren, useRef } from 'react'
 import { useQuery, useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
 import { FetchPoolProps } from './pool.types'
 import { getNetworkConfig } from '@/lib/config/app.config'
 import { useMandatoryContext } from '@/lib/shared/utils/contexts'
+import { useApolloClient } from '@apollo/client'
 
 export type UsePoolResponse = ReturnType<typeof _usePool>
 export const PoolContext = createContext<UsePoolResponse | null>(null)
@@ -36,7 +37,27 @@ export function _usePool({ id, chain, variant }: FetchPoolProps) {
   return { pool, loading, refetch }
 }
 
-export function PoolProvider({ id, chain, variant, children }: PropsWithChildren<FetchPoolProps>) {
+export function PoolProvider({
+  id,
+  chain,
+  variant,
+  children,
+  initialQuery,
+}: PropsWithChildren<FetchPoolProps> & { initialQuery: GetPoolQuery }) {
+  const loaded = useRef(false)
+  const client = useApolloClient()
+
+  if (!loaded.current) {
+    console.log('loading data')
+    client.writeQuery({
+      query: GetPoolDocument,
+      data: initialQuery,
+      variables: { id },
+    })
+
+    loaded.current = true
+  }
+
   const hook = _usePool({ id, chain, variant })
   return <PoolContext.Provider value={hook}>{children}</PoolContext.Provider>
 }
