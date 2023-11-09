1f144fefd88d7c0b7210122e4239952407a6bce9
Alberto Gualis <agualis@gmail.com>
2023-11-09 19:43:32 +0100

    Add chakra link

--- app/(app)/debug/page.tsx
+++ app/(app)/debug/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 import { Heading, VStack } from '@chakra-ui/react'
-import Link from 'next/link'
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
 
 export default function Debug() {
   return (
     <VStack padding="lg" margin="lg">
       <Heading size="md">Demos</Heading>
-      <Link href="/debug/relayer-approval">Relayer approval</Link>
-      <Link href="/debug/native-asset">Native asset (ETH) join</Link>
-      <Link href="/debug/token-approval">Join with token approval</Link>
+      {/* <Link href="/debug/relayer-approval">Relayer approval</Link>
+      <Link href="/debug/native-asset">Native asset (ETH) join</Link> */}
+      <Link as={NextLink} href="/debug/token-approval" color="blue">
+        Join with token approval
+      </Link>
     </VStack>
   )
 }
