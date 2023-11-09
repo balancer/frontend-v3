2a7d7ee5f06a5d814707bcd8b4246b3e9955fdc8
Ansaf Ahmed <me@ansaf.net>
2023-11-10 01:02:01 +1100

    fix: build

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,7 +11,7 @@ export function Providers({ children }: { children: ReactNode }) {
   const initialColorMode = cookies().get(COLOR_MODE_STORAGE_KEY)?.value
 
   return (
-    <ThemeProvider initialColorMode={initialColorMode}>
+    <ThemeProvider initialColorMode={initialColorMode as 'light' | 'dark' | 'system'}>
       <Web3Provider>
         <ApolloProviderWrapper>
           <TokensProvider>
