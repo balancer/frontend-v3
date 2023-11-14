79c4f714ab906593b666674574919242cb7287e8
Gareth Fuller <gareth.a.f@gmail.com>
2023-11-14 11:41:14 +0000

    chore: Some comments

--- lib/shared/hooks/useNumbers.ts
+++ lib/shared/hooks/useNumbers.ts
@@ -70,6 +70,9 @@ export function useNumbers() {
   const { currency } = useUserSettings()
   const { getFxRate, hasFxRates } = useFxRates()
 
+  /**
+   * Converts a USD value to the user's currency value.
+   */
   function toUserCurrency(usdVal: Numberish): string {
     const amount = usdVal.toString()
     const fxRate = getFxRate(currency)
@@ -79,6 +82,9 @@ export function useNumbers() {
 
   type CurrencyOpts = { withSymbol?: boolean; abbreviated?: boolean }
 
+  /**
+   * Converts a USD value to the user's currency and formats in fiat style.
+   */
   function toCurrency(
     usdVal: Numberish,
     { withSymbol = true, abbreviated = true }: CurrencyOpts = {}
