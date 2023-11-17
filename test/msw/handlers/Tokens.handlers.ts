90470de55cb8442b4bd4e71461752fcdc40305b6
Alberto Gualis <agualis@gmail.com>
2023-11-17 06:26:21 +0100

    Add MSW handlers for GetTokenPricesDocument

--- test/msw/handlers/Tokens.handlers.ts
+++ test/msw/handlers/Tokens.handlers.ts
@@ -1,7 +1,13 @@
-import { GetTokensDocument } from '@/lib/shared/services/api/generated/graphql'
+import {
+  GetTokenPricesDocument,
+  GetTokensDocument,
+} from '@/lib/shared/services/api/generated/graphql'
 import { graphql } from 'msw'
 import { getQueryName, mockGQL } from '../utils'
-import { defaultTokenListMock } from '@/lib/modules/tokens/__mocks__/token.builders'
+import {
+  defaultGetTokenPricesQueryMock,
+  defaultTokenListMock,
+} from '@/lib/modules/tokens/__mocks__/token.builders'
 
 export function buildTokenListMswHandler(tokenList = defaultTokenListMock) {
   return graphql.query(getQueryName(GetTokensDocument), (req, res, ctx) => {
@@ -11,4 +17,14 @@ export function buildTokenListMswHandler(tokenList = defaultTokenListMock) {
 
 export function mockTokenList(tokenList = defaultTokenListMock) {
   mockGQL(buildTokenListMswHandler(tokenList))
+}
+
+export function buildTokenPricesMswHandler(tokenList = defaultGetTokenPricesQueryMock) {
+  return graphql.query(getQueryName(GetTokenPricesDocument), (req, res, ctx) => {
+    return res(ctx.data({ tokens: tokenList }))
+  })
+}
+
+export function mockTokenPricesList(tokenPricesList = defaultGetTokenPricesQueryMock) {
+  mockGQL(buildTokenPricesMswHandler(tokenPricesList))
 }
