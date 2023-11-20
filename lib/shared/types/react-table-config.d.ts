6e16c61870faac35441cc83d7ba0f0947fd0f5d4
groninge <groninge@gmail.com>
2023-11-20 08:23:05 +0100

    format file

--- lib/shared/types/react-table-config.d.ts
+++ lib/shared/types/react-table-config.d.ts
@@ -1,22 +1,24 @@
 // https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-table
 import {
-    UseExpandedHooks,
-    UseExpandedInstanceProps,
-    UseExpandedOptions,
-    UseExpandedRowProps,
-    UseExpandedState,
-} from 'react-table';
+  UseExpandedHooks,
+  UseExpandedInstanceProps,
+  UseExpandedOptions,
+  UseExpandedRowProps,
+  UseExpandedState,
+} from 'react-table'
 
 declare module 'react-table' {
-    export interface TableOptions<D extends Record<string, unknown>> extends UseExpandedOptions<D> {}
+  export type TableOptions<D extends Record<string, unknown>> = UseExpandedOptions<D>
 
-    export interface Hooks<D extends Record<string, unknown> = Record<string, unknown>> extends UseExpandedHooks<D> {}
+  export type Hooks<D extends Record<string, unknown> = Record<string, unknown>> =
+    UseExpandedHooks<D>
 
-    export interface TableInstance<D extends Record<string, unknown> = Record<string, unknown>>
-        extends UseExpandedInstanceProps<D> {}
+  export type TableInstance<D extends Record<string, unknown> = Record<string, unknown>> =
+    UseExpandedInstanceProps<D>
 
-    export interface TableState<D extends Record<string, unknown> = Record<string, unknown>>
-        extends UseExpandedState<D> {}
+  export type TableState<D extends Record<string, unknown> = Record<string, unknown>> =
+    UseExpandedState<D>
 
-    export interface Row<D extends Record<string, unknown> = Record<string, unknown>> extends UseExpandedRowProps<D> {}
+  export type Row<D extends Record<string, unknown> = Record<string, unknown>> =
+    UseExpandedRowProps<D>
 }
