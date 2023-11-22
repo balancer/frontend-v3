33707077b89928fd7b5948717a3e1bc01e00bf4b
Gareth Fuller <gareth.a.f@gmail.com>
2023-11-22 10:07:36 +0000

    chore: Move orderedTokens into hook

--- lib/modules/tokens/TokenSelectModal/TokenSelectList/TokenSelectList.tsx
+++ lib/modules/tokens/TokenSelectModal/TokenSelectList/TokenSelectList.tsx
@@ -3,14 +3,12 @@
 import { Box, BoxProps, Center, Text } from '@chakra-ui/react'
 import VirtualList from 'react-tiny-virtual-list'
 import { TokenSelectListRow } from './TokenSelectListRow'
-import { isSameAddress } from '@/lib/shared/utils/addresses'
 import { GqlToken } from '@/lib/shared/services/api/generated/graphql'
 import { useTokenBalances } from '../../useTokenBalances'
 import { useUserAccount } from '@/lib/modules/web3/useUserAccount'
-import { orderBy } from 'lodash'
-import { useTokens } from '../../useTokens'
 import { useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
+import { useTokenSelectList } from './useTokenSelectList'
 
 type Props = {
   tokens: GqlToken[]
@@ -33,54 +31,12 @@ export function TokenSelectList({
   const [activeIndex, setActiveIndex] = useState(0)
   const { balanceFor, isBalancesLoading } = useTokenBalances(tokens)
   const { isConnected } = useUserAccount()
-  const { usdValueForToken, exclNativeAssetFilter, nativeAssetFilter } = useTokens()
-
-  const symbolMatch = (token: GqlToken, searchTerm: string) =>
-    token.symbol.toLowerCase().includes(searchTerm.toLowerCase())
-
-  const nameMatch = (token: GqlToken, searchTerm: string) =>
-    token.name.toLowerCase().includes(searchTerm.toLowerCase())
-
-  const getFilteredTokens = () => {
-    let filteredTokens = tokens
-
-    if (excludeNativeAsset) {
-      filteredTokens = filteredTokens.filter(exclNativeAssetFilter)
-    }
-
-    if (searchTerm) {
-      filteredTokens = filteredTokens.filter(token => {
-        return (
-          isSameAddress(token.address, searchTerm) ||
-          symbolMatch(token, searchTerm) ||
-          nameMatch(token, searchTerm)
-        )
-      })
-    }
-
-    return filteredTokens
-  }
-
-  let orderedTokens = orderBy(
-    getFilteredTokens(),
-    [
-      token => {
-        const userBalance = balanceFor(token)
-        return userBalance ? Number(usdValueForToken(token, userBalance?.formatted || 0)) : 0
-      },
-      'priority',
-      'symbol',
-    ],
-    ['desc', 'desc']
+  const { orderedTokens } = useTokenSelectList(
+    tokens,
+    excludeNativeAsset,
+    pinNativeAsset,
+    searchTerm
   )
-
-  if (pinNativeAsset) {
-    const nativeAsset = orderedTokens.find(nativeAssetFilter)
-
-    if (nativeAsset) {
-      orderedTokens = [nativeAsset, ...orderedTokens.filter(exclNativeAssetFilter)]
-    }
-  }
 
   const decrementActiveIndex = () => setActiveIndex(prev => Math.max(prev - 1, 0))
   const incrementActiveIndex = () =>
