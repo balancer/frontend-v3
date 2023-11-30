c5dc2562ca2130bdc32cc9010612aa6daa2841ef
Gareth Fuller <gareth.a.f@gmail.com>
2023-11-30 11:22:28 +0000

    chore: Add chain icon

--- app/(app)/pools/[chain]/[variant]/[id]/add-liquidity/page.tsx
+++ app/(app)/pools/[chain]/[variant]/[id]/add-liquidity/page.tsx
@@ -1,10 +1,29 @@
 'use client'
 
+import { getNetworkConfig } from '@/lib/config/app.config'
 import { PoolActionsLayout } from '@/lib/modules/pool/actions/PoolActionsLayout'
 import { usePool } from '@/lib/modules/pool/usePool'
+import { CloseIcon } from '@chakra-ui/icons'
+import { Card, HStack, IconButton } from '@chakra-ui/react'
+import Image from 'next/image'
 
 export default function AddLiquidityPage() {
   const { pool } = usePool()
+  const networkConfig = getNetworkConfig(pool.chain)
 
-  return <PoolActionsLayout pool={pool}>Content</PoolActionsLayout>
+  return (
+    <PoolActionsLayout pool={pool}>
+      <HStack>
+        <Card variant="level3" p="sm">
+          <Image
+            src={networkConfig.iconPath}
+            width="30"
+            height="30"
+            alt={networkConfig.shortName}
+          />
+          <IconButton isRound={true} variant="solid" aria-label="Close" icon={<CloseIcon />} />
+        </Card>
+      </HStack>
+    </PoolActionsLayout>
+  )
 }
