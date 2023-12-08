c476d00059dda668c6b184a78a0dda565b2d0cbb
groninge <groninge@gmail.com>
2023-12-08 09:50:26 +0100

    add component

---
+++ lib/shared/components/inputs/InputWithSlider/InputWithSlider.tsx
@@ -0,0 +1,93 @@
+'use client'
+
+import {
+  Box,
+  BoxProps,
+  HStack,
+  Input,
+  InputProps,
+  Text,
+  forwardRef,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from '@chakra-ui/react'
+import { blockInvalidNumberInput } from '@/lib/shared/utils/numbers'
+
+type Props = {
+  value?: string
+  boxProps?: BoxProps
+  onChange?: (event: { currentTarget: { value: string } }) => void
+}
+
+export const InputWithSlider = forwardRef(
+  ({ value, boxProps, onChange, ...inputProps }: InputProps & Props, ref) => {
+    return (
+      <>
+        <HStack w="full" justifyContent="space-between">
+          <Text>Amount</Text>
+          <Text>100%</Text>
+        </HStack>
+        <Box
+          borderRadius="md"
+          p="md"
+          shadow="innerBase"
+          bg="background.card.level1"
+          border="white"
+          w="full"
+          ref={ref}
+          {...boxProps}
+        >
+          <HStack align="start" spacing="md">
+            <Box position="relative">
+              <Input
+                type="number"
+                placeholder="0.00"
+                autoComplete="off"
+                autoCorrect="off"
+                min={0}
+                border="transparent"
+                bg="transparent"
+                shadow="none"
+                p="0"
+                fontSize="xl"
+                fontWeight="medium"
+                value={value}
+                title={String(value)}
+                onKeyDown={blockInvalidNumberInput}
+                _hover={{
+                  borderColor: 'transparent',
+                  boxShadow: 'none',
+                }}
+                _focus={{
+                  outline: 'none',
+                  borderColor: 'transparent',
+                  boxShadow: 'none',
+                }}
+                {...inputProps}
+              />
+              <Box
+                position="absolute"
+                bgGradient="linear(to-r, transparent, background.card.level1 70%)"
+                w="8"
+                h="full"
+                top={0}
+                right={0}
+                zIndex={9999}
+              ></Box>
+            </Box>
+            <Box w="50%" alignSelf="center">
+              <Slider aria-label="slider" defaultValue={30}>
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </Box>
+          </HStack>
+        </Box>
+      </>
+    )
+  }
+)
