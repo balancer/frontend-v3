5049597bbc1bc43672d2291499f74ae3cbcd8c02
Gareth Fuller <gareth.a.f@gmail.com>
2023-12-12 13:53:45 +0000

    chore: Cleanup config files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs")
+const { sentryOptions, sentryWebpackPluginOptions } = require('./lib/config/sentry/sentry.config')
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
@@ -22,47 +24,8 @@ const nextConfig = {
   pageExtensions: ['tsx', `${process.env.PROTOCOL}.tsx`],
 }
 
-module.exports = nextConfig
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
 module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "balancer-labs",
-    project: "frontend-v3",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
+  nextConfig,
+  sentryWebpackPluginOptions,
+  sentryOptions
 );
