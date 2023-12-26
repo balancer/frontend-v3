125b2746008692cb870c339a81c016eb29034fe3
Alberto Gualis <agualis@gmail.com>
2023-12-26 17:11:45 +0100

    Bump rtl utils

--- test/utils/promises.ts
+++ test/utils/promises.ts
@@ -1,3 +1,10 @@
+import { act } from 'react-dom/test-utils'
+
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// awaits ms milliseconds in the context of a hook test
+export async function actSleep(ms: number) {
+  return await act(() => sleep(ms))
 }
