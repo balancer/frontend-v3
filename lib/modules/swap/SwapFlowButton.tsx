e72f28c403d91dec317f06f3d49fe93b92756edd
Gareth Fuller <gareth.a.f@gmail.com>
2024-01-13 13:13:10 +0000

    chore: Remove comments

--- lib/modules/swap/SwapFlowButton.tsx
+++ lib/modules/swap/SwapFlowButton.tsx
@@ -1,21 +1,8 @@
 import { Button, Tooltip } from '@chakra-ui/react'
 import { useSwap } from './useSwap'
-// import { useNextTokenApprovalStep } from '../tokens/approvals/useNextTokenApprovalStep'
-// import { TokenAmountToApprove } from '../tokens/approvals/approval-rules'
 
 export function SwapFlowButton() {
   const { isDisabled, isLoading } = useSwap()
-
-  // const amountToApprove: TokenAmountToApprove = {
-  //   rawAmount: '0x',
-  //   humanAmount: '0x',
-  //   tokenAddress: '0x',
-  //   tokenSymbol: '0x',
-  // }
-
-  // const { tokenApprovalStep, initialAmountsToApprove } = useNextTokenApprovalStep(
-  //   helpers.getAmountsToApprove(humanAmountsInWithTokenInfo)
-  // )
 
   function submit() {
     console.log('swap')
