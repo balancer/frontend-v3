4cb378c03d9292c1df9d973b6b24a1bd5aba4080
Alberto Gualis <agualis@gmail.com>
2024-01-25 15:59:06 +0100

    clean step type

--- lib/shared/components/btns/transaction-steps/lib.tsx
+++ lib/shared/components/btns/transaction-steps/lib.tsx
@@ -28,9 +28,6 @@ type StepType =
   | 'tokenApproval'
   | 'addLiquidity'
   | 'removeLiquidity'
-  | 'tokenApproval'
-  | 'addLiquidity'
-  | 'removeLiquidity'
   | 'gaugeDeposit'
   | 'gaugeWithdraw'
 
