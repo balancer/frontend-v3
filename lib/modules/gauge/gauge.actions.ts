438cb703de24b0b7973078ce51a65cc58998ea7b
mrchickenburger <40786269+mrchickenburger@users.noreply.github.com>
2024-01-26 01:51:26 +1100

    Gauge actions transaction steps (#232)
    * init: gauge actions steps
    
    * update: utilise useActiveStep
    
    * update: integrate isSuccess on isCompelte
    
    * renamed for consistency
    
    * add gauge contract address
    
    * change to arrow function
    
    * add optional contract address
    
    * remove from config
    
    * pass in mandatory contract address
    
    * fix test
    
    * re-order inputs
    
    * re-order inputs here too :)
    
    ---------
    
    Co-authored-by: groninge <groninge@gmail.com>

---
+++ lib/modules/gauge/gauge.actions.ts
@@ -0,0 +1,75 @@
+import { GqlPoolStakingGauge } from '@/lib/shared/services/api/generated/graphql'
+import { useManagedTransaction } from '../web3/contracts/useManagedTransaction'
+import {
+  TransactionLabels,
+  TransactionStep,
+} from '@/lib/shared/components/btns/transaction-steps/lib'
+import { useActiveStep } from '@/lib/shared/hooks/transaction-flows/useActiveStep'
+
+function buildGaugeDepositLabels(gauge?: GqlPoolStakingGauge | null): TransactionLabels {
+  const labels: TransactionLabels = {
+    init: 'TODO DEPOSIT INIT',
+    tooltip: 'TODO DEPOSIT TOOLTIP',
+  }
+  return labels
+}
+
+function buildGaugeWithdrawLabels(gauge?: GqlPoolStakingGauge | null): TransactionLabels {
+  const labels: TransactionLabels = {
+    init: 'TODO WITHDRAW INNIT',
+    tooltip: 'TODO WITHDRAW TOOLTIP',
+  }
+  return labels
+}
+
+export function useConstructGaugeDepositActionStep(
+  gauge?: GqlPoolStakingGauge | null,
+  depositAmount?: bigint
+): TransactionStep {
+  const { activateStep } = useActiveStep()
+  const transactionLabels = buildGaugeDepositLabels(gauge)
+  const deposit = useManagedTransaction(
+    gauge?.gaugeAddress || '',
+    'balancer.gaugeV5',
+    'deposit',
+    transactionLabels,
+    { args: [depositAmount || 0n] },
+    { enabled: !!gauge || !!depositAmount }
+  )
+
+  const step: TransactionStep = {
+    ...deposit,
+    id: `${gauge?.gaugeAddress}-deposit`,
+    stepType: 'gaugeDeposit',
+    transactionLabels,
+    activateStep,
+    isComplete: () => deposit.result.isSuccess,
+  }
+  return step
+}
+
+export function useConstructGaugeWithdrawActionStep(
+  gauge?: GqlPoolStakingGauge | null,
+  withdrawAmount?: bigint
+): TransactionStep {
+  const { activateStep } = useActiveStep()
+  const transactionLabels = buildGaugeWithdrawLabels(gauge)
+  const withdraw = useManagedTransaction(
+    gauge?.gaugeAddress || '',
+    'balancer.gaugeV5',
+    'withdraw',
+    transactionLabels,
+    { args: [withdrawAmount || 0n] },
+    { enabled: !!gauge || !!withdrawAmount }
+  )
+
+  const step: TransactionStep = {
+    ...withdraw,
+    id: `${gauge?.gaugeAddress}-withdraw`,
+    stepType: 'gaugeWithdraw',
+    transactionLabels,
+    activateStep,
+    isComplete: () => withdraw.result.isSuccess,
+  }
+  return step
+}
--- lib/modules/web3/contracts/AbiMap.ts
+++ lib/modules/web3/contracts/AbiMap.ts
@@ -1,5 +1,6 @@
-import { balancerV2VaultABI } from './abi/generated'
+import { balancerV2GaugeV5ABI, balancerV2VaultABI } from './abi/generated'
 
 export const AbiMap = {
   'balancer.vaultV2': balancerV2VaultABI,
+  'balancer.gaugeV5': balancerV2GaugeV5ABI,
 }
