f23396e16552e03d25be220f3db19ffde8a3430b
Gareth Fuller <gareth.a.f@gmail.com>
2024-01-30 11:26:26 +0000

    chore: Remove redundant loading state

--- lib/modules/pool/PoolList/PoolListTable/PoolListTable.tsx
+++ lib/modules/pool/PoolList/PoolListTable/PoolListTable.tsx
@@ -32,38 +32,19 @@ export function PoolListTable({ pools, count, loading }: Props) {
   }
 
   return (
-    <Box w="full" style={{ position: 'relative' }}>
-      <PaginatedTable
-        items={pools}
-        loading={loading}
-        renderTableHeader={() => <PoolListTableHeader {...rowProps} />}
-        renderTableRow={(item: PoolListItem, index) => {
-          return <PoolListTableRow keyValue={index} pool={item} {...rowProps} />
-        }}
-        showPagination={showPagination}
-        paginationProps={paginationProps}
-        border="1px solid"
-        borderColor="border.base"
-        w="full"
-        alignItems="flex-start"
-      />
-      {loading && (
-        <Box
-          style={{
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            background: 'black',
-            top: 0,
-            left: 0,
-            opacity: 0.3,
-            borderRadius: 10,
-          }}
-        ></Box>
-      )}
-    </Box>
+    <PaginatedTable
+      items={pools}
+      loading={loading}
+      renderTableHeader={() => <PoolListTableHeader {...rowProps} />}
+      renderTableRow={(item: PoolListItem, index) => {
+        return <PoolListTableRow keyValue={index} pool={item} {...rowProps} />
+      }}
+      showPagination={showPagination}
+      paginationProps={paginationProps}
+      border="1px solid"
+      borderColor="border.base"
+      w="full"
+      alignItems="flex-start"
+    />
   )
 }
