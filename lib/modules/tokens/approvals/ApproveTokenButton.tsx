6f747ebe46163673a2e1de3eac5ab87c8f73e466
Alberto Gualis <agualis@gmail.com>
2024-02-01 07:52:29 +0100

    Remove old imports

--- lib/modules/tokens/approvals/ApproveTokenButton.tsx
+++ lib/modules/tokens/approvals/ApproveTokenButton.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { TransactionStepButton } from '@/lib/shared/components/btns/transaction-steps/TransactionStepButton'
-import { useAddLiquidity } from '../../pool/actions/add-liquidity/useAddLiquidity'
 import { ApproveTokenProps, useConstructApproveTokenStep } from './useConstructApproveTokenStep'
 import { CommonStepProps } from '../../pool/actions/useIterateSteps'
 
