c2356b6fa9eb2cb95457697f7332fc2fa81731d9
Gareth Fuller <gareth.a.f@gmail.com>
2024-02-09 11:21:33 +0000

    chore: init

---
+++ lib/modules/pool/actions/add-liquidity/handlers/ProportionalAddLiquidity.handler.ts
@@ -0,0 +1,82 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { getDefaultRpcUrl } from '@/lib/modules/web3/Web3Provider'
+import { TransactionConfig } from '@/lib/modules/web3/contracts/contract.types'
+import {
+  AddLiquidity,
+  AddLiquidityKind,
+  AddLiquidityUnbalancedInput,
+  Slippage,
+} from '@balancer/sdk'
+import { Pool } from '../../../usePool'
+import { LiquidityActionHelpers } from '../../LiquidityActionHelpers'
+import { HumanAmountIn } from '../../liquidity-types'
+import { AddLiquidityHandler } from './AddLiquidity.handler'
+import { SdkBuildAddLiquidityInput, SdkQueryAddLiquidityOutput } from '../add-liquidity.types'
+
+/**
+ * ProportionalAddLiquidityHandler is a handler that implements the
+ * AddLiquidityHandler interface for unbalanced adds, e.g. where the user
+ * specifies the token amounts in. It uses the Balancer SDK to implement it's
+ * methods. It also handles the case where one of the input tokens is the native
+ * asset instead of the wrapped native asset.
+ */
+export class ProportionalAddLiquidityHandler implements AddLiquidityHandler {
+  helpers: LiquidityActionHelpers
+
+  constructor(pool: Pool) {
+    this.helpers = new LiquidityActionHelpers(pool)
+  }
+
+  public async simulate(humanAmountsIn: HumanAmountIn[]): Promise<SdkQueryAddLiquidityOutput> {
+    const addLiquidity = new AddLiquidity()
+    const { bptOut } = calculateProportionalAmounts(this.helpers.poolStateWithBalance, amountIn)
+    const addLiquidityInput = this.constructSdkInput(humanAmountsIn)
+
+    const sdkQueryOutput = await addLiquidity.query(addLiquidityInput, this.helpers.poolState)
+
+    return { bptOut: sdkQueryOutput.bptOut, sdkQueryOutput }
+  }
+
+  public async getPriceImpact(): Promise<number> {
+    return 0 // Proportional joins don't have price impact
+  }
+
+  public async buildCallData({
+    account,
+    slippagePercent,
+    queryOutput,
+  }: SdkBuildAddLiquidityInput): Promise<TransactionConfig> {
+    const addLiquidity = new AddLiquidity()
+
+    const { call, to, value } = addLiquidity.buildCall({
+      chainId: this.helpers.chainId,
+      ...queryOutput.sdkQueryOutput,
+      slippage: Slippage.fromPercentage(`${Number(slippagePercent)}`),
+      sender: account,
+      recipient: account,
+    })
+
+    return {
+      account,
+      chainId: this.helpers.chainId,
+      data: call,
+      to,
+      value,
+    }
+  }
+
+  /**
+   * PRIVATE METHODS
+   */
+  private constructSdkInput(humanAmountsIn: HumanAmountIn[]): AddLiquidityUnbalancedInput {
+    const amountsIn = this.helpers.toInputAmounts(humanAmountsIn)
+
+    return {
+      chainId: this.helpers.chainId,
+      rpcUrl: getDefaultRpcUrl(this.helpers.chainId),
+      amountsIn,
+      kind: AddLiquidityKind.Unbalanced,
+      useNativeAssetAsWrappedAmountIn: this.helpers.isNativeAssetIn(humanAmountsIn),
+    }
+  }
+}
