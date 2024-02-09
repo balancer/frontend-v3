f7db89f6bada43ec712d8521877be233659a980d
Alberto Gualis <agualis@gmail.com>
2024-02-09 17:11:55 +0100

    Debug GHA 2

--- test/anvil/anvil-global-setup.ts
+++ test/anvil/anvil-global-setup.ts
@@ -1,6 +1,6 @@
 import { startFork } from './anvil-runner'
 
 export default async function globalSetup() {
-  startFork('POLYGON', { verbose: true })
-  await startFork('MAINNET', { verbose: true })
+  // startFork('POLYGON', { verbose: true })
+  // await startFork('MAINNET', { verbose: true })
 }
