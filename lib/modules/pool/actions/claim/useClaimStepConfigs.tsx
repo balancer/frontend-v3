3436792dc890f2b56abd85904ced9962374da6f7
groninge <groninge@gmail.com>
2024-02-15 08:54:01 +0100

    fix props

--- lib/modules/pool/actions/claim/useClaimStepConfigs.tsx
+++ lib/modules/pool/actions/claim/useClaimStepConfigs.tsx
@@ -4,15 +4,25 @@ import { approveRelayerConfig } from '@/lib/modules/relayer/approveRelayerConfig
 import { minterApprovalConfig } from '@/lib/modules/staking/gauge/minterApprovalConfig'
 import { ClaimAllRewardsButton } from './ClaimAllRewardsButton'
 import { Address } from 'viem'
-import { Pool } from '../../usePool'
+import { GqlChain, GqlPoolStakingType } from '@/lib/shared/services/api/generated/graphql'
 
-export function useClaimStepConfigs(gaugeAddresses: Address[], pool: Pool) {
+export function useClaimStepConfigs(
+  gaugeAddresses: Address[],
+  chain: GqlChain,
+  stakingType: GqlPoolStakingType
+) {
   const { hasMinterApproval } = useHasMinterApproval()
   const { hasApprovedRelayer } = useHasApprovedRelayer()
 
   let stepConfigs = [
     {
-      render: () => <ClaimAllRewardsButton gaugeAddresses={gaugeAddresses} pool={pool} />,
+      render: () => (
+        <ClaimAllRewardsButton
+          gaugeAddresses={gaugeAddresses}
+          chain={chain}
+          stakingType={stakingType}
+        />
+      ),
     },
   ]
 
--- lib/modules/pool/actions/claim/useClaiming.tsx
+++ lib/modules/pool/actions/claim/useClaiming.tsx
@@ -8,6 +8,7 @@ import { useIterateSteps } from '../useIterateSteps'
 import { useClaimStepConfigs } from './useClaimStepConfigs'
 import { Address } from 'viem'
 import { Pool } from '../../usePool'
+import { GqlPoolStakingType } from '@/lib/shared/services/api/generated/graphql'
 
 export function useClaiming(gaugeAddresses: Address[], pool: Pool) {
   const { isConnected } = useUserAccount()
@@ -16,7 +17,11 @@ export function useClaiming(gaugeAddresses: Address[], pool: Pool) {
     LABELS.walletNotConnected,
   ])
 
-  const stepConfigs = useClaimStepConfigs(gaugeAddresses, pool)
+  const stepConfigs = useClaimStepConfigs(
+    gaugeAddresses,
+    pool.chain,
+    pool.staking?.type || GqlPoolStakingType.Gauge
+  )
   const { currentStep, useOnStepCompleted } = useIterateSteps(stepConfigs)
 
   return {
