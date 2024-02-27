6394e9e2e0aad6703bf144b16f7eace1e08c2f00
Alberto Gualis <alberto@balancer.finance>
2024-02-27 09:38:04 +0100

    refactor: avoid global reactive humanAmountInVar in useStaking (#310)

--- lib/modules/pool/actions/stake/useStaking.tsx
+++ lib/modules/pool/actions/stake/useStaking.tsx
@@ -4,21 +4,20 @@
 import { isDisabledWithReason } from '@/lib/shared/utils/functions/isDisabledWithReason'
 import { useUserAccount } from '@/lib/modules/web3/useUserAccount'
 import { LABELS } from '@/lib/shared/labels'
-import { makeVar, useReactiveVar } from '@apollo/client'
 import { HumanAmountIn } from '../liquidity-types'
 import { Address, parseUnits } from 'viem'
 import { useTokenAllowances } from '@/lib/modules/web3/useTokenAllowances'
 import { usePool } from '../../usePool'
 import { BPT_DECIMALS } from '../../pool.constants'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTokenApprovalConfigs } from '@/lib/modules/tokens/approvals/useTokenApprovalConfigs'
 import { stakeConfig } from './stakeConfig'
 import { useIterateSteps } from '../useIterateSteps'
 import { getChainId } from '@/lib/config/app.config'
 
-export const humanAmountInVar = makeVar<HumanAmountIn | null>(null)
-
 export function useStaking() {
+  const [humanAmountIn, setHumanAmountIn] = useState<HumanAmountIn | null>(null)
+
   const { userAddress, isConnected } = useUserAccount()
   const { pool } = usePool()
   const { isDisabled, disabledReason } = isDisabledWithReason([
@@ -26,15 +25,13 @@ export function useStaking() {
     LABELS.walletNotConnected,
   ])
 
-  const humanAmountIn = useReactiveVar(humanAmountInVar)
-
   function setInitialHumanAmountIn() {
     const amountIn = {
       tokenAddress: pool.address,
       humanAmount: pool.userBalance?.walletBalance,
     } as HumanAmountIn
 
-    humanAmountInVar(amountIn)
+    setHumanAmountIn(amountIn)
   }
 
   const tokenAllowances = useTokenAllowances({
