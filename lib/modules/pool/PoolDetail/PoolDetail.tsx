b0bd04970931783847997f8bf62db51f4181741a
Gareth Fuller <gareth.a.f@gmail.com>
2024-02-28 12:54:04 +0000

    chore: Make PoolDetail a server component

--- lib/modules/pool/PoolDetail/PoolDetail.tsx
+++ lib/modules/pool/PoolDetail/PoolDetail.tsx
@@ -7,7 +7,7 @@ import { PoolAccordion } from './PoolAccordion/PoolAccordion'
 import PoolMetaBadges from './PoolMetaBadges/PoolMetaBadges'
 import { PoolChart } from './PoolChart/PoolChart'
 
-export function PoolDetail() {
+export async function PoolDetail() {
   return (
     <Stack width="full">
       {/* {loading && <Text>Loading...</Text>} */}
