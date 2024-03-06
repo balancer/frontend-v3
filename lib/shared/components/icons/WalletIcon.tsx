8dfeeb1489fa976ab311205d080531acdbafd289
Alberto Gualis <agualis@gmail.com>
2024-03-06 19:32:01 +0100

    chore: camel case svg icon attributes

--- lib/shared/components/icons/WalletIcon.tsx
+++ lib/shared/components/icons/WalletIcon.tsx
@@ -6,11 +6,11 @@ export function WalletIcon({ size = 24 }: { size?: number }) {
       width={size}
       height={size}
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
