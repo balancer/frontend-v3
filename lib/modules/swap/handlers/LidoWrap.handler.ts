6435880cfc36da84da554453ec1adba0d6f2ce37
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-12 09:30:55 +0000

    chore: Make build function work

--- lib/modules/swap/handlers/LidoWrap.handler.ts
+++ lib/modules/swap/handlers/LidoWrap.handler.ts
@@ -1,13 +1,17 @@
-import { getChainId, getNetworkConfig } from '@/lib/config/app.config'
+import { getChainId } from '@/lib/config/app.config'
 import { SwapHandler } from './Swap.handler'
 import { TransactionConfig } from '../../web3/contracts/contract.types'
 import { SdkBuildSwapInputs, SimulateSwapInputs, SimulateSwapResponse } from '../swap.types'
-import { WrapType, getWrapType } from '../useWrapping'
-import { encodeFunctionData, formatUnits } from 'viem'
+import { WrapType, getWrapConfig, getWrapType } from '../useWrapping'
+import { Address, encodeFunctionData, formatUnits } from 'viem'
 import { Hex } from 'viem'
 import { bn } from '@/lib/shared/utils/numbers'
 import { getViemClient } from '@/lib/shared/services/viem/viem.client'
 import { GqlChain } from '@/lib/shared/services/api/generated/graphql'
+
+const lidoRateProviderMap: Partial<Record<GqlChain, Address>> = {
+  [GqlChain.Mainnet]: '0x72d07d7dca67b8a406ad1ec34ce969c90bfee768',
+}
 
 export class LidoWrapHandler implements SwapHandler {
   async simulate({ ...variables }: SimulateSwapInputs): Promise<SimulateSwapResponse> {
@@ -29,11 +33,11 @@ export class LidoWrapHandler implements SwapHandler {
     const wrapType = getWrapType(tokenIn.address, tokenOut.address, selectedChain)
     if (!wrapType) throw new Error('Non valid wrap tokens')
 
-    const { tokens } = getNetworkConfig(selectedChain)
+    const { wrappedToken } = getWrapConfig(tokenIn.address, tokenOut.address, selectedChain)
 
     let data: Hex | undefined
     if (wrapType === WrapType.WRAP) {
-      data = this.buildWrapCallData()
+      data = this.buildWrapCallData(tokenIn.scaledAmount)
     } else if (wrapType === WrapType.UNWRAP) {
       data = this.buildUnwrapCallData(tokenIn.scaledAmount)
     }
@@ -44,11 +48,11 @@ export class LidoWrapHandler implements SwapHandler {
       account,
       chainId: getChainId(selectedChain),
       data,
-      to: tokens.addresses.wNativeAsset,
+      to: wrappedToken,
     }
   }
 
-  private buildWrapCallData() {
+  private buildWrapCallData(scaledAmount: bigint) {
     return encodeFunctionData({
       abi: [
         {
@@ -60,6 +64,7 @@ export class LidoWrapHandler implements SwapHandler {
         },
       ],
       functionName: 'wrap',
+      args: [scaledAmount],
     })
   }
 
@@ -67,20 +72,23 @@ export class LidoWrapHandler implements SwapHandler {
     return encodeFunctionData({
       abi: [
         {
-          inputs: [{ name: 'amount', type: 'uint256' }],
-          name: 'withdraw',
+          inputs: [{ name: '_wstETHAmount', type: 'uint256' }],
+          name: 'unwrap',
           outputs: [{ name: 'amount', type: 'uint256' }],
           stateMutability: '',
           type: 'function',
         },
       ],
-      functionName: 'withdraw',
+      functionName: 'unwrap',
       args: [scaledAmount],
     })
   }
 
   private async applyRate(amount: string, wrapType: WrapType | null, chain: GqlChain) {
     const viemClient = getViemClient(chain)
+    const rateProviderAddress = lidoRateProviderMap[chain]
+
+    if (!rateProviderAddress) throw new Error('No rate provider for chain')
 
     const rateScaled = await viemClient.readContract({
       abi: [
@@ -92,7 +100,7 @@ export class LidoWrapHandler implements SwapHandler {
           type: 'function',
         },
       ],
-      address: '0x72d07d7dca67b8a406ad1ec34ce969c90bfee768',
+      address: rateProviderAddress,
       functionName: 'getRate',
     })
 
