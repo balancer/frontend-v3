4bb5c612904ace3b55cd38ce62fa132f4459e983
Alberto Gualis <alberto@balancer.finance>
2024-03-13 18:15:41 +0100

    feat: desktop step tracker (#370)
    * feat: add desktop version of StepTracker
    
    * detect last completed step

--- lib/modules/pool/actions/add-liquidity/AddLiquidityModal.tsx
+++ lib/modules/pool/actions/add-liquidity/AddLiquidityModal.tsx
@@ -13,6 +13,7 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalContentProps,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
@@ -34,6 +35,9 @@ import TokenRow from '@/lib/modules/tokens/TokenRow/TokenRow'
 import { useUserSettings } from '@/lib/modules/user/settings/useUserSettings'
 import { SignRelayerButton } from '@/lib/modules/transactions/transaction-steps/SignRelayerButton'
 import { useShouldSignRelayerApproval } from '@/lib/modules/relayer/signRelayerApproval.hooks'
+import { MobileStepTracker } from '@/lib/modules/transactions/transaction-steps/step-tracker/MobileStepTracker'
+import { DesktopStepTracker } from '@/lib/modules/transactions/transaction-steps/step-tracker/DesktopStepTracker'
+import { useResponsive } from '@/lib/shared/hooks/useResponsive'
 
 type Props = {
   isOpen: boolean
@@ -48,6 +52,7 @@ export function AddLiquidityModal({
   finalFocusRef,
   ...rest
 }: Props & Omit<ModalProps, 'children'>) {
+  const { isDesktop, isMobile } = useResponsive()
   const initialFocusRef = useRef(null)
   const {
     humanAmountsIn,
@@ -57,6 +62,8 @@ export function AddLiquidityModal({
     tokens,
     addLiquidityTxState,
     currentStep,
+    currentStepIndex,
+    stepConfigs,
     useOnStepCompleted,
   } = useAddLiquidity()
   const { toCurrency } = useCurrency()
@@ -70,6 +77,8 @@ export function AddLiquidityModal({
   const priceImpact = priceImpactQuery?.data
   const priceImpactLabel = priceImpact !== undefined ? fNum('priceImpact', priceImpact) : '-'
 
+  const modalStyles: ModalContentProps = isDesktop ? { left: '-100px', position: 'relative' } : {}
+
   return (
     <Modal
       isOpen={isOpen}
@@ -80,7 +89,14 @@ export function AddLiquidityModal({
       {...rest}
     >
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent {...modalStyles}>
+        {isDesktop && (
+          <DesktopStepTracker
+            currentStepIndex={currentStepIndex}
+            stepConfigs={stepConfigs}
+            chainId={chainId}
+          />
+        )}
         <ModalHeader>
           <Heading fontWeight="bold" size="h5">
             Add liquidity
@@ -89,6 +105,15 @@ export function AddLiquidityModal({
         <ModalCloseButton />
         <ModalBody>
           <VStack spacing="md" align="start">
+            {isMobile && (
+              <Card variant="level5" p="md" shadow="sm" w="full">
+                <MobileStepTracker
+                  currentStepIndex={currentStepIndex}
+                  stepConfigs={stepConfigs}
+                  chainId={chainId}
+                />
+              </Card>
+            )}
             <Card variant="level3" p="md" shadow="sm" w="full">
               <VStack align="start" spacing="md">
                 <HStack justify="space-between" w="full">
