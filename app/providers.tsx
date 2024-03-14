a22dafa33ac0678d093e66777b93ec9fb24b60cc
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-14 20:31:27 +0000

    disable cookies

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,27 +6,25 @@ import { RecentTransactionsProvider } from '@/lib/modules/transactions/RecentTra
 import { CurrentFlowStepProvider } from '@/lib/modules/transactions/transaction-steps/useCurrentFlowStep'
 import { ApolloGlobalDataProvider } from '@/lib/shared/services/api/apollo-global-data.provider'
 import { UserSettingsProvider } from '@/lib/modules/user/settings/useUserSettings'
-import { COOKIE_KEYS } from '@/lib/modules/cookies/cookie.constants'
 import { ReactQueryClientProvider } from './react-query.provider'
-import { cookies } from 'next/headers'
 
 export function Providers({ children }: { children: ReactNode }) {
-  const initialColorMode = cookies().get(COOKIE_KEYS.UserSettings.ColorMode)?.value
-  const initCurrency = cookies().get(COOKIE_KEYS.UserSettings.Currency)?.value
-  const initSlippage = cookies().get(COOKIE_KEYS.UserSettings.Slippage)?.value
-  const initEnableSignatures = cookies().get(COOKIE_KEYS.UserSettings.EnableSignatures)?.value
-  const initPoolListView = cookies().get(COOKIE_KEYS.UserSettings.PoolListView)?.value
+  // const initialColorMode = cookies().get(COOKIE_KEYS.UserSettings.ColorMode)?.value
+  // const initCurrency = cookies().get(COOKIE_KEYS.UserSettings.Currency)?.value
+  // const initSlippage = cookies().get(COOKIE_KEYS.UserSettings.Slippage)?.value
+  // const initEnableSignatures = cookies().get(COOKIE_KEYS.UserSettings.EnableSignatures)?.value
+  // const initPoolListView = cookies().get(COOKIE_KEYS.UserSettings.PoolListView)?.value
 
   return (
-    <ThemeProvider initialColorMode={initialColorMode as 'light' | 'dark' | 'system'}>
+    <ThemeProvider initialColorMode={'system'}>
       <Web3Provider>
         <ApolloClientProvider>
           <ApolloGlobalDataProvider>
             <UserSettingsProvider
-              initCurrency={initCurrency}
-              initSlippage={initSlippage}
-              initEnableSignatures={initEnableSignatures}
-              initPoolListView={initPoolListView}
+              initCurrency={undefined}
+              initSlippage={undefined}
+              initEnableSignatures={undefined}
+              initPoolListView={undefined}
             >
               <CurrentFlowStepProvider>
                 <RecentTransactionsProvider>
