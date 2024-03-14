bc6bec235c0dc22864c36ec09b038787cdb58db7
Daniel <danielBeets@protonmail.com>
2024-03-14 08:36:58 +0100

    disable cookies, fix build errors

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,6 @@ import { Web3Provider } from '@/lib/modules/web3/Web3Provider'
 import { ApolloClientProvider } from '@/lib/shared/services/api/apollo-client-provider'
 import { ThemeProvider } from '@/lib/shared/services/chakra/ThemeProvider'
 import { ReactNode } from 'react'
-import { cookies } from 'next/headers'
 import { RecentTransactionsProvider } from '@/lib/modules/transactions/RecentTransactionsProvider'
 import { ApolloGlobalDataProvider } from '@/lib/shared/services/api/apollo-global-data.provider'
 import { UserSettingsProvider } from '@/lib/modules/user/settings/useUserSettings'
@@ -10,22 +9,22 @@ import { COOKIE_KEYS } from '@/lib/modules/cookies/cookie.constants'
 import { ReactQueryClientProvider } from './react-query.provider'
 
 export function Providers({ children }: { children: ReactNode }) {
-  const initialColorMode = cookies().get(COOKIE_KEYS.UserSettings.ColorMode)?.value
+  /*const initialColorMode = cookies().get(COOKIE_KEYS.UserSettings.ColorMode)?.value
   const initCurrency = cookies().get(COOKIE_KEYS.UserSettings.Currency)?.value
   const initSlippage = cookies().get(COOKIE_KEYS.UserSettings.Slippage)?.value
   const initEnableSignatures = cookies().get(COOKIE_KEYS.UserSettings.EnableSignatures)?.value
-  const initPoolListView = cookies().get(COOKIE_KEYS.UserSettings.PoolListView)?.value
+  const initPoolListView = cookies().get(COOKIE_KEYS.UserSettings.PoolListView)?.value*/
 
   return (
-    <ThemeProvider initialColorMode={initialColorMode as 'light' | 'dark' | 'system'}>
+    <ThemeProvider initialColorMode={'light'}>
       <Web3Provider>
         <ApolloClientProvider>
           <ApolloGlobalDataProvider>
             <UserSettingsProvider
-              initCurrency={initCurrency}
-              initSlippage={initSlippage}
-              initEnableSignatures={initEnableSignatures}
-              initPoolListView={initPoolListView}
+              initCurrency={undefined}
+              initSlippage={undefined}
+              initEnableSignatures={undefined}
+              initPoolListView={undefined}
             >
               <RecentTransactionsProvider>
                 <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
