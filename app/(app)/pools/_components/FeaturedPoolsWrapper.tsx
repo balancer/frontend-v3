24555970ef7414934e3634d9ded2ecc9a3a896da
Daniel <danielBeets@protonmail.com>
2024-03-15 06:44:48 +0100

    increase revalidate times

--- app/(app)/pools/_components/FeaturedPoolsWrapper.tsx
+++ app/(app)/pools/_components/FeaturedPoolsWrapper.tsx
@@ -14,7 +14,7 @@ export async function FeaturedPoolsWrapper() {
     variables,
     context: {
       fetchOptions: {
-        next: { revalidate: 30 },
+        next: { revalidate: 300 },
       },
     },
   })
--- app/(app)/pools/_components/PoolListWrapper.tsx
+++ app/(app)/pools/_components/PoolListWrapper.tsx
@@ -50,7 +50,7 @@ export default async function PoolListWrapper({ searchParams }: Props) {
     variables,
     context: {
       fetchOptions: {
-        next: { revalidate: 30 },
+        next: { revalidate: 120 },
       },
     },
   })
