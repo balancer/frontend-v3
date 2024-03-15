46dee27d915a1eec107e1dba8bd27954def930d1
groninge <groninge@gmail.com>
2024-03-15 11:55:38 +0100

    add totalApr prop

--- lib/modules/pool/usePool.tsx
+++ lib/modules/pool/usePool.tsx
@@ -10,12 +10,10 @@ import {
 import { createContext, PropsWithChildren } from 'react'
 import { useQuery } from '@apollo/experimental-nextjs-app-support/ssr'
 import { FetchPoolProps } from './pool.types'
-import { getNetworkConfig } from '@/lib/config/app.config'
 import { useMandatoryContext } from '@/lib/shared/utils/contexts'
 import { useSeedApolloCache } from '@/lib/shared/hooks/useSeedApolloCache'
 import { calcBptPriceFor, usePoolHelpers } from './pool.helpers'
 import { useUserAccount } from '@/lib/modules/web3/useUserAccount'
-
 import { usePoolEnrichWithOnChainData } from '@/lib/modules/pool/queries/usePoolEnrichWithOnChainData'
 import { useOnchainUserPoolBalances } from './queries/useOnchainUserPoolBalances'
 
@@ -66,12 +64,18 @@ export function _usePool({
 
   const isLoading = isLoadingOnchainData || isLoadingOnchainUserBalances
 
+  const totalApr =
+    pool.dynamicData.apr.apr.__typename === 'GqlPoolAprRange'
+      ? parseFloat(pool.dynamicData.apr.apr.max)
+      : parseFloat(pool.dynamicData.apr.apr.total)
+
   return {
     pool,
     bptPrice,
     isLoading,
     isLoadingOnchainData,
     isLoadingOnchainUserBalances,
+    totalApr,
     // TODO: we assume here that we never need to reload the entire pool.
     // this assumption may need to be questioned
     refetch,
