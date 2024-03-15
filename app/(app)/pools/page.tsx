4f1afc2f331bdb2529c3a37ca106dd1287ebeb1e
Daniel <danielBeets@protonmail.com>
2024-03-15 06:46:06 +0100

    remove pool list

--- app/(app)/pools/page.tsx
+++ app/(app)/pools/page.tsx
@@ -1,11 +1,11 @@
 import { VStack } from '@chakra-ui/react'
 import { Suspense } from 'react'
 import { FeaturedPoolsWrapper } from '@/app/(app)/pools/_components/FeaturedPoolsWrapper'
-import PoolListWrapper from '@/app/(app)/pools/_components/PoolListWrapper'
+//import PoolListWrapper from '@/app/(app)/pools/_components/PoolListWrapper'
 
 export const revalidate = 30
 
-interface Props {
+/*interface Props {
   searchParams: {
     first?: string
     skip?: string
@@ -16,17 +16,17 @@ interface Props {
     textSearch?: string
     userAddress?: string
   }
-}
+}*/
 
-export default async function Pools({ searchParams }: Props) {
+export default async function Pools(/*{ searchParams }: Props*/) {
   return (
     <VStack align="start" spacing="2xl">
       <Suspense>
         <FeaturedPoolsWrapper />
       </Suspense>
-      <Suspense>
+      {/*<Suspense>
         <PoolListWrapper searchParams={searchParams} />
-      </Suspense>
+      </Suspense>*/}
     </VStack>
   )
 }
