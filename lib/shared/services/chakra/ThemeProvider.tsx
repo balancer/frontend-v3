1e0208dee7459b59694c6cc0c4ba385b117b5e2b
Daniel <danielBeets@protonmail.com>
2024-03-15 16:51:43 +0100

    remove chakra cache provider

--- lib/shared/services/chakra/ThemeProvider.tsx
+++ lib/shared/services/chakra/ThemeProvider.tsx
@@ -1,27 +1,20 @@
 'use client'
 
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import { CacheProvider as ChakraCacheProvider } from '@chakra-ui/next-js'
-import { ReactNode, useEffect, useState } from 'react'
-import theme, { balTheme } from './theme'
-import { createColorModeManager } from './colorModeManager'
+import { ChakraProvider } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import theme from './theme'
 
 export function ThemeProvider({
   children,
-  initialColorMode,
 }: {
   children: ReactNode
   initialColorMode?: 'light' | 'dark' | 'system'
 }) {
-  //const colorModeManager = createColorModeManager(initialColorMode)
-
   return (
     <>
-      <ChakraCacheProvider>
-        <ChakraProvider theme={theme} cssVarsRoot="body">
-          {children}
-        </ChakraProvider>
-      </ChakraCacheProvider>
+      <ChakraProvider theme={theme} cssVarsRoot="body">
+        {children}
+      </ChakraProvider>
     </>
   )
 }
