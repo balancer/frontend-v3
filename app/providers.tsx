ef058201210800b2543c035789ee1f115fe412f4
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-17 13:34:23 +0000

    chore: Set default theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -18,7 +18,7 @@ export function Providers({ children }: { children: ReactNode }) {
   // const initPoolListView = cookies().get(COOKIE_KEYS.UserSettings.PoolListView)?.value
 
   return (
-    <ColorThemeProvider>
+    <ColorThemeProvider defaultTheme="dark">
       <ThemeProvider>
         <Web3Provider>
           <ApolloClientProvider>
