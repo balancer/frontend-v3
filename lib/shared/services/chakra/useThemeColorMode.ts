46cc1681ee0546840dacd53e94fab9417f6f5554
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-20 09:23:04 +0000

    chore: Update type name

--- lib/shared/services/chakra/useThemeColorMode.ts
+++ lib/shared/services/chakra/useThemeColorMode.ts
@@ -1,12 +1,12 @@
 import { useTheme } from 'next-themes'
 import { DEFAULT_THEME_COLOR_MODE } from './theme'
 
-export type ThemeColor = 'light' | 'dark'
+export type ColorMode = 'light' | 'dark'
 
-export function useThemeColorMode(): ThemeColor {
+export function useThemeColorMode(): ColorMode {
   const { theme } = useTheme()
 
-  const _theme = theme as ThemeColor | undefined
+  const _theme = theme as ColorMode | undefined
 
   return _theme || DEFAULT_THEME_COLOR_MODE
 }
