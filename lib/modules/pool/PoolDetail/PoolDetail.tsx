83f2f427c57f588353e373787a058b279d572872
mrchickenburger <40786269+mrchickenburger@users.noreply.github.com>
2024-03-21 20:15:53 +1100

    Feature/updated pool detail (#345)
    * update: New <PoolDetail/> hero with zen graphics
    
    * update: Icons for the hero section
    
    * update: Big zen symbols
    
    * update: complete apr section
    
    * update: fix border radius distribution
    
    * update: Updated my liquidity
    
    * update: updating more components on the pool detail page
    
    * update: flatten the accordion
    
    * update: integrate other level variants instead of the ones introduced in current branch
    
    * update: parity with theme from main
    
    * update: light mode shadows + adjust incentive badges
    
    * update: consolidate zen garden + update incentives and meta badge styling

--- lib/modules/pool/PoolDetail/PoolDetail.tsx
+++ lib/modules/pool/PoolDetail/PoolDetail.tsx
@@ -1,34 +1,56 @@
-import { HStack, Stack, VStack } from '@chakra-ui/react'
+'use client'
+import { Grid, GridItem, Stack, VStack } from '@chakra-ui/react'
 import { PoolComposition } from './PoolComposition/PoolComposition'
 import PoolStats from './PoolStats'
 import PoolMyLiquidity from './PoolMyLiquidity'
 import PoolIncentives from './PoolIncentives'
-import { PoolAccordion } from './PoolAccordion/PoolAccordion'
 import PoolMetaBadges from './PoolMetaBadges/PoolMetaBadges'
 import { PoolChart } from './PoolChart/PoolChart'
+import { PoolActivityChart } from './PoolActivityChart/PoolActivityChart'
+import { PoolAttributes } from './PoolAttributes/PoolAttributes'
+import { PoolRisks } from './PoolRisks/PoolRisks'
+import { PoolContracts } from './PoolContracts/PoolContracts'
 import { PoolAlerts } from '../alerts/PoolAlerts'
 
 export async function PoolDetail() {
   return (
     <Stack width="full">
-      <VStack width="full" spacing="16">
-        <VStack alignItems="flex-start" width="full" spacing="5">
-          <PoolMetaBadges />
-          <PoolAlerts />
-          <PoolStats />
-          <HStack width="full" spacing="4">
-            <PoolMyLiquidity />
-            <PoolIncentives />
-          </HStack>
-          <HStack width="full" spacing="4">
-            <PoolComposition />
-          </HStack>
-          <HStack width="full" spacing="4">
-            <PoolChart />
-          </HStack>
-        </VStack>
-        <PoolAccordion />
-      </VStack>
+      {/* {loading && <Text>Loading...</Text>} */}
+      <Grid width="full" rowGap="10" columnGap="4" templateColumns="1fr 1fr">
+        <GridItem colSpan={2}>
+          <VStack alignItems="flex-start" spacing="5">
+            <PoolMetaBadges />
+            <PoolStats />
+          </VStack>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <PoolMyLiquidity />
+        </GridItem>
+        <GridItem pt="10" colSpan={2}>
+          <PoolIncentives />
+        </GridItem>
+
+        <GridItem pt="10" colSpan={1}>
+          <PoolComposition />
+        </GridItem>
+        <GridItem pt="10" colSpan={1}>
+          <PoolChart />
+        </GridItem>
+        <GridItem colSpan={2}>
+          <PoolActivityChart />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <PoolAttributes />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <VStack spacing="4">
+            <PoolRisks />
+            <PoolContracts />
+          </VStack>
+        </GridItem>
+        {/* <HStack width="full" spacing="4"></HStack> */}
+        {/* <PoolAccordion /> */}
+      </Grid>
     </Stack>
   )
 }
--- lib/modules/pool/PoolDetail/PoolRisks/PoolRisks.tsx
+++ lib/modules/pool/PoolDetail/PoolRisks/PoolRisks.tsx
@@ -8,7 +8,7 @@ export function PoolRisks() {
   const risks = getPoolRisks(pool as GqlPoolElement)
 
   return (
-    <Card minHeight="175px" width="full" variant="level2" px="6" py="5">
+    <Card variant="level2" shadow="2xl" width="full" px="4" py="5" borderWidth={0}>
       <VStack alignItems="flex-start" spacing="4" width="full">
         <Heading variant="h4" fontSize="1.25rem">
           Pool risks
