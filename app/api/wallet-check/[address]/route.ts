2a23d9959b9653f0587a72d72449282e39e56088
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-21 14:45:35 +0000

    chore: Fix typecheck

--- app/api/wallet-check/[address]/route.ts
+++ app/api/wallet-check/[address]/route.ts
@@ -1,4 +1,5 @@
 import { hours } from '@/lib/shared/hooks/useTime'
+import { NextResponse } from 'next/server'
 
 // Caches response from Hypernative for 12 hours *per address*.
 export const revalidate = hours(12).toSecs()
@@ -19,7 +20,7 @@ type ReputationResponse = {
 const IGNORED_FLAGS = ['F-1402', 'F-1412', 'F-3110']
 
 export async function GET(request: Request, { params: { address } }: Params) {
-  if (!hypernativeApiKey) return Response.json({ data: { isAuthorized: true } })
+  if (!hypernativeApiKey) return NextResponse.json({ data: { isAuthorized: true } })
 
   try {
     const res = await fetch('https://api.hypernative.xyz/assets/reputation/addresses', {
@@ -39,8 +40,8 @@ export async function GET(request: Request, { params: { address } }: Params) {
 
     const isAuthorized = check.flags.some(flag => !IGNORED_FLAGS.includes(flag))
 
-    return Response.json({ data: { ...check, isAuthorized } })
+    return NextResponse.json({ data: { ...check, isAuthorized } })
   } catch (error) {
-    return Response.json({ data: { isAuthorized: true } })
+    return NextResponse.json({ data: { isAuthorized: true } })
   }
 }
