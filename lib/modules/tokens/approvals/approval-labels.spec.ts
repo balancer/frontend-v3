531204f112fe8b931f35e9bc131633475f777f24
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-26 09:54:27 +0000

    chore: Fix tests

--- lib/modules/tokens/approvals/approval-labels.spec.ts
+++ lib/modules/tokens/approvals/approval-labels.spec.ts
@@ -5,7 +5,9 @@ test('Token approval labels for add liquidity', () => {
   const result = buildTokenApprovalLabels(args)
   expect(result).toMatchInlineSnapshot(`
     {
+      "confirmed": "WETH approved!",
       "confirming": "Approving WETH",
+      "error": "Error approving WETH",
       "init": "Approve WETH for adding liquidity",
       "tooltip": "You must approve WETH to add liquidity for this token on Balancer.
     Approvals are required once per token, per wallet.",
@@ -18,7 +20,9 @@ test('Token approval labels for unapprove', () => {
   const result = buildTokenApprovalLabels(args)
   expect(result).toMatchInlineSnapshot(`
     {
+      "confirmed": "WETH unapproved",
       "confirming": "Unapproving WETH",
+      "error": "Error unapproving WETH",
       "init": "Unapprove WETH",
       "tooltip": "You must unapprove WETH before a new approval value can be set.",
     }
