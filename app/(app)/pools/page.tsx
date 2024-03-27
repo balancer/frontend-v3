e5e6a1748ec651b018db7c3325e7e9ec1d181f18
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-27 14:56:03 +0000

    chore: Add skeleton for the suspense boundary

--- app/(app)/pools/page.tsx
+++ app/(app)/pools/page.tsx
@@ -1,12 +1,13 @@
 import { FeaturedPools } from '@/lib/modules/featured-pools/FeaturedPools'
 import { PoolList } from '@/lib/modules/pool/PoolList/PoolList'
+import { Skeleton } from '@chakra-ui/react'
 import { Suspense } from 'react'
 
 export default async function Pools() {
   return (
     <>
       <FeaturedPools mb="2xl" />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Skeleton w="full" h="500px" />}>
         <PoolList />
       </Suspense>
     </>
