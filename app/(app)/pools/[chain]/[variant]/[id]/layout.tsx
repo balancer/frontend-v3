d61ef9ce0db9d10566c558dd63d80563fe830d3f
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-27 12:33:48 +0000

    chore: Add PoolDetail loading state

--- app/(app)/pools/[chain]/[variant]/[id]/layout.tsx
+++ app/(app)/pools/[chain]/[variant]/[id]/layout.tsx
@@ -2,6 +2,7 @@ import { FetchPoolProps } from '@/lib/modules/pool/pool.types'
 import { ChainSlug } from '@/lib/modules/pool/pool.utils'
 import { PropsWithChildren, Suspense } from 'react'
 import PoolLoader from '@/lib/modules/pool/PoolLoader'
+import { PoolDetail } from '@/lib/modules/pool/PoolDetail/PoolDetail'
 
 type Props = PropsWithChildren<{
   params: Omit<FetchPoolProps, 'chain'> & { chain: ChainSlug }
@@ -9,7 +10,7 @@ type Props = PropsWithChildren<{
 
 export default async function PoolLayout({ params: { id, chain, variant }, children }: Props) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PoolDetail isLoading />}>
       <PoolLoader id={id} chain={chain} variant={variant}>
         {children}
       </PoolLoader>
