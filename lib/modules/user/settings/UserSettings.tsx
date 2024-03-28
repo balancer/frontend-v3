5e0ef8e104f1679d251845306b38ed6e1765c421
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-28 20:06:54 +0000

    chore: Update UserSettings

--- lib/modules/user/settings/UserSettings.tsx
+++ lib/modules/user/settings/UserSettings.tsx
@@ -13,7 +13,6 @@ import {
   PopoverBody,
   PopoverCloseButton,
   PopoverContent,
-  PopoverHeader,
   PopoverTrigger,
   VStack,
   Text,
@@ -87,11 +86,14 @@ export function UserSettings() {
       <PopoverContent>
         <PopoverArrow bg="background.level3" />
         <PopoverCloseButton />
-        <PopoverHeader>
-          <Heading size="md">Settings</Heading>
-        </PopoverHeader>
-        <PopoverBody>
-          <VStack align="start" spacing="24px" py="3">
+        <PopoverBody p="0">
+          <HStack color="font.special" p="md" pb="0">
+            <Settings size={20} />
+            <Heading size="md" variant="special">
+              Settings
+            </Heading>
+          </HStack>
+          <VStack align="start" spacing="24px" p="md">
             <Box w="full">
               <Heading size="sm" pb="2">
                 Currency
