e30babf4a46a66f97b4dfdfed7b9dab4c977b81f
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-29 11:34:56 +0000

    chore: Remove custom padding

--- lib/modules/pool/PoolDetail/PoolRisks/PoolRisks.tsx
+++ lib/modules/pool/PoolDetail/PoolRisks/PoolRisks.tsx
@@ -32,7 +32,7 @@ export function RisksList({ textVariant = 'secondary' }: RisksListProps) {
 
 export function PoolRisks() {
   return (
-    <Card width="full" px="4" py="5">
+    <Card width="full">
       <VStack alignItems="flex-start" spacing="4" width="full">
         <Heading variant="h4" fontSize="1.25rem">
           Pool risks
