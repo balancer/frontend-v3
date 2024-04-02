64277bbeb9c7d3ff2bd4b63f919715560da25ab4
Gareth Fuller <gareth.a.f@gmail.com>
2024-04-02 16:11:02 +0100

    chore: Make breakpoints work for server rendered components

--- lib/shared/hooks/useBreakpoints.tsx
+++ lib/shared/hooks/useBreakpoints.tsx
@@ -1,7 +1,10 @@
 import { useBreakpointValue } from '@chakra-ui/react'
 
 export function useBreakpoints() {
-  const isMobile = useBreakpointValue({ base: true, lg: false }, { ssr: true })
+  // Counter intuitive, but setting ssr to false will make the hook work as
+  // expected with server components...
+  const isMobile = useBreakpointValue({ base: true, lg: false }, { ssr: false })
+
   return {
     isMobile,
     isDesktop: !isMobile,
