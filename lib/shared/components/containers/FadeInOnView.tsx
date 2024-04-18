1a735b7a0119d1ee3b67c4e109e135be321dddf8
uiuxxx <uxuidefi@gmail.com>
2024-04-18 17:33:01 +0100

    Remove Chakra breakpoints

--- lib/shared/components/containers/FadeInOnView.tsx
+++ lib/shared/components/containers/FadeInOnView.tsx
@@ -2,7 +2,6 @@
 
 import { useRef } from 'react'
 import { useInView } from 'framer-motion'
-import { useBreakpointValue } from '@chakra-ui/react'
 
 interface FadeInOnViewProps {
   children: React.ReactNode
@@ -13,14 +12,8 @@ const FadeInOnView: React.FC<FadeInOnViewProps> = ({ children, animateOnce = tru
   const ref = useRef(null)
   const isInView = useInView(ref, { once: animateOnce })
 
-  // Using Chakra UI's useBreakpointValue to adjust the CSS class based on the breakpoint
-  const scaleClass = useBreakpointValue({ base: 'scale-base', md: 'scale-md' }) || 'scale-base'
-
-  const opacityClass =
-    useBreakpointValue({ base: 'opacity-base', md: 'opacity-md' }) || 'opacity-base'
-
   return (
-    <div ref={ref} className={`${isInView ? 'visible' : 'hidden'} ${scaleClass} ${opacityClass}`}>
+    <div ref={ref} className={`${isInView ? 'visible' : 'hidden'} fade-in-opacity fade-in-scale`}>
       {children}
     </div>
   )
