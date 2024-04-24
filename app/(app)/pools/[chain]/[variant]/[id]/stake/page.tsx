ad43fdeec94cf1c093ad9efcff90b3aa336d47f9
Gareth Fuller <gareth.a.f@gmail.com>
2024-04-24 21:02:01 +0100

    chore: Block access to staking page under conditions

--- app/(app)/pools/[chain]/[variant]/[id]/stake/page.tsx
+++ app/(app)/pools/[chain]/[variant]/[id]/stake/page.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import { PoolActionsLayout } from '@/lib/modules/pool/actions/PoolActionsLayout'
 import { StakeForm } from '@/lib/modules/pool/actions/stake/StakeForm'
+import { usePoolRedirect } from '@/lib/modules/pool/pool.hooks'
+import { usePool } from '@/lib/modules/pool/usePool'
+import { bn } from '@/lib/shared/utils/numbers'
+import { useLayoutEffect } from 'react'
 
 export default function StakePage() {
+  const { pool } = usePool()
+  const { redirectToPoolPage } = usePoolRedirect(pool)
+
+  const hasBalance = bn(pool.userBalance?.walletBalance || '0').gt(0)
+
+  const canStake = pool.staking && hasBalance
+
+  useLayoutEffect(() => {
+    if (!canStake) redirectToPoolPage()
+  }, [])
+
+  if (!canStake) return null
+
   return (
     <PoolActionsLayout>
       <StakeForm />
