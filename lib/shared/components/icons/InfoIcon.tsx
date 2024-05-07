bd0db4968286a734989cef6668fde25f7c22a73b
Alberto Gualis <agualis@gmail.com>
2024-05-07 10:52:27 +0200

    chore: avoid forwardRef warning

--- lib/shared/components/icons/InfoIcon.tsx
+++ lib/shared/components/icons/InfoIcon.tsx
@@ -1,9 +1,9 @@
 import { Box, BoxProps, forwardRef } from '@chakra-ui/react'
 import { Info } from 'react-feather'
 
-export const InfoIcon = forwardRef(({ ...props }: BoxProps) => {
+export const InfoIcon = forwardRef<BoxProps, 'div'>((props, ref) => {
   return (
-    <Box color="grayText" {...props}>
+    <Box color="grayText" ref={ref} {...props}>
       <Info size={16} />
     </Box>
   )
