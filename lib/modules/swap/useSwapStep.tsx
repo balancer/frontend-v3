1fc7f1674f042c768a01437a7485ad55fa59363f
Alberto Gualis <agualis@gmail.com>
2024-05-10 10:42:23 +0200

    chore: refetch balances when swap transaction is successful

--- lib/modules/swap/useSwapStep.tsx
+++ lib/modules/swap/useSwapStep.tsx
@@ -34,8 +34,6 @@ export function useSwapStep({
   tokenOutInfo,
 }: SwapStepParams): TransactionStep2 {
   const [isBuildQueryEnabled, setIsBuildQueryEnabled] = useState(false)
-  //TODO: This throws
-  // const { refetchBalances } = useTokenBalances()
   const { getTransaction } = useTransactionState()
   const buildSwapQuery = useBuildSwapQuery({
     handler,
@@ -84,10 +82,6 @@ export function useSwapStep({
       isComplete,
       onActivated: () => setIsBuildQueryEnabled(true),
       onDeactivated: () => setIsBuildQueryEnabled(false),
-      onSuccess: () => {
-        console.log('Swap successful: refetching balances...')
-        // refetchBalances()
-      },
       renderAction: () => (
         <VStack w="full">
           <ManagedSendTransactionButton
