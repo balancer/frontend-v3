b4ce8716f7c6074e8af371470ad95f382e83613b
Alberto Gualis <agualis@gmail.com>
2024-05-13 16:18:48 +0200

    refactor: move build query from provider to step

--- lib/modules/pool/actions/remove-liquidity/modal/useRemoveLiquiditySteps.tsx
+++ lib/modules/pool/actions/remove-liquidity/modal/useRemoveLiquiditySteps.tsx
@@ -1,27 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { useShouldSignRelayerApproval } from '@/lib/modules/relayer/signRelayerApproval.hooks'
 import { useApproveRelayerStep } from '@/lib/modules/relayer/useApproveRelayerStep'
 import { useRelayerMode } from '@/lib/modules/relayer/useRelayerMode'
-import { usePool } from '../../../usePool'
 import { useSignRelayerStep } from '@/lib/modules/transactions/transaction-steps/SignRelayerButton'
-import { useShouldSignRelayerApproval } from '@/lib/modules/relayer/signRelayerApproval.hooks'
 import { TransactionStep } from '@/lib/modules/transactions/transaction-steps/lib'
-import { RemoveLiquiditySimulationQueryResult } from '../queries/useRemoveLiquiditySimulationQuery'
-import { RemoveLiquidityBuildQueryResponse } from '../queries/useRemoveLiquidityBuildCallDataQuery'
-import { useRemoveLiquidityStep } from './useRemoveLiquidityStep'
 import { useMemo } from 'react'
+import { usePool } from '../../../usePool'
 import { shouldUseRecoveryRemoveLiquidity } from '../../LiquidityActionHelpers'
+import { RemoveLiquidityStepParams, useRemoveLiquidityStep } from './useRemoveLiquidityStep'
 
-export function useRemoveLiquiditySteps(
-  simulationQuery: RemoveLiquiditySimulationQueryResult,
-  buildCallDataQuery: RemoveLiquidityBuildQueryResponse
-): TransactionStep[] {
+export function useRemoveLiquiditySteps(params: RemoveLiquidityStepParams): TransactionStep[] {
   const relayerMode = useRelayerMode()
   const { chainId, pool } = usePool()
   const shouldSignRelayerApproval = useShouldSignRelayerApproval(chainId)
   const signRelayerStep = useSignRelayerStep()
   const approveRelayerStep = useApproveRelayerStep(chainId)
 
-  const removeLiquidityStep = useRemoveLiquidityStep(simulationQuery, buildCallDataQuery)
+  const removeLiquidityStep = useRemoveLiquidityStep(params)
 
   return useMemo(() => {
     if (relayerMode === 'approveRelayer') {
