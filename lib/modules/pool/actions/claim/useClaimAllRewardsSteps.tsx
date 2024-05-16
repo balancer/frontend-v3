a365d3dbe4b67d9c3a4320bdebf21ce140980eed
Alberto Gualis <agualis@gmail.com>
2024-05-16 12:51:53 +0200

    fix: always show minter and relayer approbals in stake/unstake steps

--- lib/modules/pool/actions/claim/useClaimAllRewardsSteps.tsx
+++ lib/modules/pool/actions/claim/useClaimAllRewardsSteps.tsx
@@ -1,6 +1,4 @@
 import { getChainId } from '@/lib/config/app.config'
-import { useHasApprovedRelayer } from '@/lib/modules/relayer/useHasApprovedRelayer'
-import { useHasMinterApproval } from '@/lib/modules/staking/gauge/useHasMinterApproval'
 import { useApproveMinterStep } from '@/lib/modules/staking/gauge/useMinterApprovalStep'
 import { TransactionStep } from '@/lib/modules/transactions/transaction-steps/lib'
 import { useMemo } from 'react'
@@ -11,8 +9,6 @@ export function useClaimAllRewardsSteps(params: ClaimAllRewardsStepParams) {
   const { chain } = params.pools[0]
   const chainId = getChainId(chain)
 
-  const { hasMinterApproval } = useHasMinterApproval()
-  const { hasApprovedRelayer } = useHasApprovedRelayer(chainId)
   const approveRelayerStep = useApproveRelayerStep(chainId)
   const { step: minterApprovalStep, isLoading: isLoadingMinterApprovalStep } =
     useApproveMinterStep(chain)
@@ -21,17 +17,8 @@ export function useClaimAllRewardsSteps(params: ClaimAllRewardsStepParams) {
     useClaimAllRewardsStep(params)
 
   const steps = useMemo((): TransactionStep[] => {
-    const _steps = [claimAllRewardsStep]
-    if (!hasApprovedRelayer) _steps.unshift(approveRelayerStep)
-    if (!hasMinterApproval) _steps.unshift(minterApprovalStep)
-    return _steps
-  }, [
-    approveRelayerStep,
-    claimAllRewardsStep,
-    hasApprovedRelayer,
-    hasMinterApproval,
-    minterApprovalStep,
-  ])
+    return [minterApprovalStep, approveRelayerStep, claimAllRewardsStep]
+  }, [approveRelayerStep, claimAllRewardsStep, minterApprovalStep])
 
   return {
     isLoading: isLoadingMinterApprovalStep || isLoadingClaimAllRewards,
