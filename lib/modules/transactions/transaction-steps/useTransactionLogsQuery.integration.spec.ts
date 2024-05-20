f58a6da770b801ae7785089045be94d9d8b3e976
Alberto Gualis <agualis@gmail.com>
2024-05-20 20:34:46 +0200

    chore: remove old test

--- lib/modules/transactions/transaction-steps/useTransactionLogsQuery.integration.spec.ts
+++ lib/modules/transactions/transaction-steps/useTransactionLogsQuery.integration.spec.ts
@@ -80,9 +80,8 @@ test('queries remove liquidity transaction', async () => {
 })
 
 test('queries swap transaction', async () => {
-  // https://polygonscan.com/tx/0x11380dcffb24c512da18f032d9f7354d154cfda6bbab0633df182fcd202c4244
-
   const userAddress = '0xf76142b79Db34E57852d68F9c52C0E24f7349647'
+  // https://polygonscan.com/tx/0x11380dcffb24c512da18f032d9f7354d154cfda6bbab0633df182fcd202c4244
   const txHash = '0x11380dcffb24c512da18f032d9f7354d154cfda6bbab0633df182fcd202c4244'
 
   const result = await testSwapReceipt(userAddress, txHash, GqlChain.Polygon)
@@ -98,29 +97,6 @@ test('queries swap transaction', async () => {
     humanAmount: '1.419839650912753603',
     tokenAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
   })
-})
-test.only('queries swap transaction2', async () => {
-  // https://polygonscan.com/tx/0x11380dcffb24c512da18f032d9f7354d154cfda6bbab0633df182fcd202c4244
-
-  const userAddress = defaultTestUserAccount
-  const txHash = '0x60ea9d764740d4ec3992297c4267e76859c6ef1931689ab7f2a231fa7698e654'
-
-  const result = await testSwapReceipt(userAddress, txHash, GqlChain.Mainnet)
-
-  await waitFor(() => expect(result.current.isLoading).toBeFalsy())
-
-  console.log(result.current.sentToken)
-  console.log(result.current.receivedToken)
-
-  // expect(result.current.sentToken).toEqual({
-  //   humanAmount: '1',
-  //   tokenAddress: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
-  // })
-
-  // expect(result.current.receivedToken).toEqual({
-  //   humanAmount: '1.419839650912753603',
-  //   tokenAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
-  // })
 })
 
 test('returns is loading when user is not provided', async () => {
