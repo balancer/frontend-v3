7aed52492333b068471228aad35c498349aa79b1
Alberto Gualis <agualis@gmail.com>
2024-05-20 21:58:29 +0200

    chore: Remove slow test option

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -7,21 +7,12 @@ function setupFilesWithoutMswSetup() {
   return setupFiles.filter(file => file !== 'test/vitest/setup-msw.ts')
 }
 
-const isCI = true
-const exclude = (tag: string) => {
-  console.warn('Excluding tests tagged with', tag)
-  console.log('\x1b[33m Welcome to the app! \x1b[0m')
-  return `^((?!${tag}).)*$`
-}
-
 const integrationTestOptions: Partial<InlineConfig> = {
   include: ['./**/*.integration.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   // Avoid msw in integration tests
   setupFiles: [...setupFilesWithoutMswSetup(), 'test/vitest/setup-integration.ts'],
   globalSetup: ['./test/anvil/anvil-global-setup.ts'],
   testTimeout: 30_000,
-  // Exclude slow tests in CI: TODO: add ci env
-  testNamePattern: isCI ? exclude('slow') : undefined,
   // Consider disabling threads if we detect problems with anvil
   // poolOptions: {
   //   threads: { singleThread: true },
