cff9b2ee5bfd048d4f423ca33b88bc97455757e4
Alberto Gualis <agualis@gmail.com>
2024-05-22 06:48:35 +0200

    chore: fix comment

--- lib/modules/pool/actions/add-liquidity/queries/useAddLiquidityPriceImpactQuery.ts
+++ lib/modules/pool/actions/add-liquidity/queries/useAddLiquidityPriceImpactQuery.ts
@@ -53,7 +53,7 @@ export function useAddLiquidityPriceImpactQuery({ handler, humanAmountsIn, enabl
  ABA priceImpact calculation has some known limitations. Examples:
  - You can’t calculate it for add liquidity amounts that are higher than the pool balance (it will return a BAL#001 error)
  - Add Liquidity Unbalanced uses a querySwap and Weighted pools have a limit that you can’t swap > 30% of the token balance in the pool
- for add liquidity amounts that are higher than the pool balance (it will return a BAL#001 error)
+ for add liquidity amounts that are higher than the pool balance (it will return a BAL#304 error)
 
  For now, if we receive a ContractFunctionExecutionError we will assume that it is an ABA limitation and we will show an "unknown price impact" warning to the user.
 
