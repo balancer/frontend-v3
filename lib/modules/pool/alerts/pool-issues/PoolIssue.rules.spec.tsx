1f9f9e944e92e9aa48b8676eecc1751bedfef9df
Anastasios <anastas3993@gmail.com>
2024-06-06 12:43:00 +0400

    fix: tests

--- lib/modules/pool/alerts/pool-issues/PoolIssue.rules.spec.tsx
+++ lib/modules/pool/alerts/pool-issues/PoolIssue.rules.spec.tsx
@@ -1,45 +1,29 @@
 /* eslint-disable max-len */
 import { recoveryPoolMock } from '../../__mocks__/recoveryPoolMock'
-import { getPoolIssueAlerts } from './PoolIssue.rules'
+import { getNetworkPoolAlerts } from './PoolIssue.rules'
 
 describe('Creates pool alerts for', () => {
   test('a pool with 2 vulnerability alerts', () => {
-    expect(getPoolIssueAlerts(recoveryPoolMock)).toMatchInlineSnapshot(`
+    expect(getNetworkPoolAlerts(recoveryPoolMock)).toMatchInlineSnapshot(`
       [
         {
-          "id": "eulerRecoveryModeWarning",
-          "labels": [],
+          "identifier": "eulerRecoveryModeWarning",
+          "isSoftWarning": false,
+          "learnMoreLink": "https://docs.balancer.fi/concepts/governance/emergency.html",
           "status": "error",
-          "title": <div>
-            Due to an exploit on Euler, this pool has been set to recovery mode by the 
-             
-            <a
-              href="https://docs.balancer.fi/concepts/governance/emergency.html"
-              rel="noreferrer"
-              target="blank"
-            >
-              Emergency multisig
-            </a>
+          "title": <React.Fragment>
+            Due to an exploit on Euler, this pool has been set to recovery mode by the Emergency multisig
             . Proportional withdrawals are enabled in the UI and you are encouraged to withdraw as soon as possible.
-          </div>,
-          "type": "Vulnerability",
+          </React.Fragment>,
         },
         {
-          "id": "cspPoolVulnWarning",
-          "labels": [],
+          "identifier": "cspPoolVulnWarning",
+          "isSoftWarning": false,
+          "learnMoreLink": "https://forum.balancer.fi/t/vulnerability-found-in-some-pools/5102/1",
           "status": "error",
-          "title": <div>
-            A vulnerability has been discovered that affects this pool. Existing liquidity providers should remove liquidity immediately. 
-            <a
-              href="https://forum.balancer.fi/t/vulnerability-found-in-some-pools/5102/1"
-              rel="noreferrer"
-              target="blank"
-            >
-              Read more
-            </a>
-            .
-          </div>,
-          "type": "Vulnerability",
+          "title": <React.Fragment>
+            A vulnerability has been discovered that affects this pool. Existing liquidity providers should remove liquidity immediately.
+          </React.Fragment>,
         },
       ]
     `)
