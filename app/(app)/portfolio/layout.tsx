7109126ee744ff34b76874d049203dcd4cbd8955
Gareth Fuller <gareth.a.f@gmail.com>
2024-06-27 11:58:11 +0100

    feat: Add CoW theme (#656)
    * chore: Init
    
    * chore: Move page container further down the stack
    
    * chore: Update cow colors
    
    * chore: Update noisy card bg color
    
    * fix: Border colors
    
    * chore: Move footer outside container

--- app/(app)/portfolio/layout.tsx
+++ app/(app)/portfolio/layout.tsx
@@ -1,9 +1,14 @@
 import { PortfolioProvider } from '@/lib/modules/portfolio/PortfolioProvider'
+import { DefaultPageContainer } from '@/lib/shared/components/containers/DefaultPageContainer'
 
 type Props = {
   children: React.ReactNode
 }
 
 export default async function PortfolioLayout({ children }: Props) {
-  return <PortfolioProvider>{children}</PortfolioProvider>
+  return (
+    <DefaultPageContainer>
+      <PortfolioProvider>{children}</PortfolioProvider>
+    </DefaultPageContainer>
+  )
 }
--- lib/shared/services/chakra/themes/cow/cow.theme.ts
+++ lib/shared/services/chakra/themes/cow/cow.theme.ts
@@ -1,12 +1,12 @@
 import { ThemeTypings, extendTheme } from '@chakra-ui/react'
 import { colors, primaryTextColor } from './colors'
-import { getTokens } from '../base/tokens'
 import { getComponents } from '../base/components'
 import { config, fonts, styles } from '../base/foundations'
-import { getSemanticTokens } from '../base/semantic-tokens'
 import { proseTheme } from '../base/prose'
+import { getSemanticTokens } from '../base/semantic-tokens'
+import { getCowTokens } from './tokens'
 
-const tokens = getTokens(colors, primaryTextColor)
+const tokens = getCowTokens(colors, primaryTextColor)
 const components = getComponents(tokens, primaryTextColor)
 const semanticTokens = getSemanticTokens(tokens, colors)
 
