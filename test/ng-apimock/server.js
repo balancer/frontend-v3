3a61394c9ee33eea60bda4fc4cde504fd469b6f5
groninge <groninge@gmail.com>
2024-07-09 08:19:47 +0200

    add cors

--- test/ng-apimock/server.js
+++ test/ng-apimock/server.js
@@ -1,18 +1,17 @@
 const apimock = require('@ng-apimock/core')
 const devInterface = require('@ng-apimock/dev-interface')
 const express = require('express')
-const app = express()
-app.set('port', 9999)
-
-app.use('/dev-interface', express.static(devInterface))
-app.use(apimock.middleware)
+const cors = require('cors')
 
 apimock.processor.process({
   src: '',
 })
 
+const app = express()
+app.use(cors())
+app.set('port', 9999)
+app.use('/dev-interface', express.static(devInterface))
 app.use(apimock.middleware)
-
 app.listen(app.get('port'), () => {
   console.log('@ng-apimock/core running on port', app.get('port'))
   console.log('@ng-apimock/dev-interface is available under /dev-interface')
