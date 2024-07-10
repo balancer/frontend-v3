478894f7e1c746203da03994c3f57cb15f7695d7
Alberto Gualis <agualis@gmail.com>
2024-07-10 17:22:03 +0200

    chore: turn alert link into button

--- lib/modules/portfolio/merkl/MerklAlert.tsx
+++ lib/modules/portfolio/merkl/MerklAlert.tsx
@@ -1,9 +1,9 @@
-import { Alert, AlertIcon, HStack, Link } from '@chakra-ui/react'
+import { Alert, AlertIcon, Button, HStack, Link } from '@chakra-ui/react'
+import { ArrowUpRight } from 'react-feather'
 
 export function MerklAlert() {
   return (
     <Alert status="info" width="full">
-      <AlertIcon />
       <MerklTitle />
     </Alert>
   )
@@ -13,11 +13,19 @@ function MerklTitle() {
   return (
     <HStack justify="space-between" w="full">
       <HStack>
+        <AlertIcon />
         <div>It looks like you could also have Merkl rewards.</div>
       </HStack>
-      <Link href="https://app.merkl.xyz/" target="_blank">
-        Go to merkl.xyz
-      </Link>
+      <Button
+        as={Link}
+        target="_blank"
+        href="https://app.merkl.xyz/"
+        width="auto"
+        variant={'outline'}
+        rightIcon={<ArrowUpRight />}
+      >
+        Learn more
+      </Button>
     </HStack>
   )
 }
