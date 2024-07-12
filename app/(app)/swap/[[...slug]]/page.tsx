52605ac61b284260c2e9e63b2f4b16e472a7d6f9
Gareth Fuller <gareth.a.f@gmail.com>
2024-07-12 16:01:36 +0100

    chore: Init

--- app/(app)/swap/[[...slug]]/page.tsx
+++ app/(app)/swap/[[...slug]]/page.tsx
@@ -1,6 +1,15 @@
-'use client'
+/* eslint-disable max-len */
 
+import { getProjectConfig } from '@/lib/config/getProjectConfig'
 import { SwapForm } from '@/lib/modules/swap/SwapForm'
+import { Metadata } from 'next'
+
+const { projectName } = getProjectConfig()
+
+export const metadata: Metadata = {
+  title: `Swap tokens on ${projectName}`,
+  description: `Swap tokens on networks like Ethereum, Optimism, Arbitrum and Base via the Balancer decentralized exchange`,
+}
 
 export default function SwapPage() {
   return <SwapForm />
