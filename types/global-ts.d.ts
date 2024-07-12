1fd1b87efb74ed8befefc011e5e82cd5cbba65d4
uiuxxx <uxuidefi@gmail.com>
2024-07-12 15:48:27 +0100

    Update type definitions to include new html popover attributes

--- types/global-ts.d.ts
+++ types/global-ts.d.ts
@@ -1,4 +1,5 @@
 import { HumanAmount } from '@balancer/sdk'
+import React from 'react'
 
 /*
 Global type overrides for TypeScript
@@ -6,4 +7,11 @@ Global type overrides for TypeScript
 declare module 'viem' {
   //Override viem's formatUnits function to return HumanAmount (AKA `${number}`) to improve type safety
   export function formatUnits(value: bigint, decimals: number): HumanAmount
+}
+
+declare module 'react' {
+  interface ButtonHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    popovertarget?: string
+    popovertargetaction?: string
+  }
 }
