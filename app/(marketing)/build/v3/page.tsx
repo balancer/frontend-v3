eaca0f8756777ddb2a598806fb654ed49147ec61
Gareth Fuller <gareth.a.f@gmail.com>
2024-07-18 13:57:08 +0100

    chore: Add ReactLenis to v3 page

--- app/(marketing)/build/v3/page.tsx
+++ app/(marketing)/build/v3/page.tsx
@@ -9,16 +9,19 @@ import { V3UseCases } from '@/lib/shared/components/marketing/build/v3/V3UseCase
 import { V3Technical } from '@/lib/shared/components/marketing/build/v3/V3Technical'
 import { V3Grants } from '@/lib/shared/components/marketing/build/v3/V3Grants'
 import { V3VideoTutorial } from '@/lib/shared/components/marketing/build/v3/V3VideoTutorial'
+import { ReactLenis } from '@studio-freight/react-lenis'
 
 export default function Home() {
   return (
-    <Box className="build">
-      <V3Hero />
-      <V3VideoTutorial />
-      <V3About />
-      <V3UseCases />
-      <V3Technical />
-      <V3Grants />
-    </Box>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
+      <Box className="build">
+        <V3Hero />
+        <V3VideoTutorial />
+        <V3About />
+        <V3UseCases />
+        <V3Technical />
+        <V3Grants />
+      </Box>
+    </ReactLenis>
   )
 }
