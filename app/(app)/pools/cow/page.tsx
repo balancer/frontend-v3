8bb1b3dc674e271bd5ae26621d29d05c6188b18f
Gareth Fuller <gareth.a.f@gmail.com>
2024-07-24 21:49:45 +0100

    chore: Adjust metadata

--- app/(app)/pools/cow/page.tsx
+++ app/(app)/pools/cow/page.tsx
@@ -4,6 +4,16 @@ import { GqlPoolType } from '@/lib/shared/services/api/generated/graphql'
 import { Skeleton } from '@chakra-ui/react'
 import { Suspense } from 'react'
 import { CowBanner } from './CowBanner'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: `CoWAMM DeFi Liquidity Pools`,
+  description: `
+    Explore DeFi liquidity pools or create your own.
+    Provide liquidity to accumulate yield from swap fees
+    while retaining your token exposure as prices move.
+  `,
+}
 
 export default function PoolsPage() {
   return (
