afd6d3555c48f8a801749f30d8d9938029152cc6
Anastasios <anastas3993@gmail.com>
2024-07-24 11:17:03 +0300

    feat: put aura staked positions in sep row (#847)
    * feat: split aura staked positions in sep row
    
    * feat: change aura icon color

--- lib/modules/portfolio/PortfolioTable/useExpandedPools.ts
+++ lib/modules/portfolio/PortfolioTable/useExpandedPools.ts
@@ -1,9 +1,11 @@
 import { useMemo } from 'react'
 import { Pool } from '../../pool/PoolProvider'
 import { isVebalPool } from '../../pool/pool.helpers'
+import { GqlPoolStakingType } from '@/lib/shared/services/api/generated/graphql'
 
 export enum ExpandedPoolType {
-  Staked = 'staked',
+  StakedBal = 'staked-bal',
+  StakedAura = 'staked-aura',
   Unstaked = 'unstaked',
   Locked = 'locked',
   Unlocked = 'unlocked',
@@ -22,18 +24,31 @@ export function useExpandedPools(pools: Pool[]) {
     pools.forEach(pool => {
       const isVeBal = isVebalPool(pool.id)
 
-      const stakedBalanceUsd =
-        pool.userBalance?.stakedBalances.reduce(
-          (acc, balance) => acc + Number(balance.balanceUsd),
-          0
-        ) || 0
+      const stakedBalancesBalUsd =
+        pool.userBalance?.stakedBalances
+          .filter(balance => balance.stakingType !== GqlPoolStakingType.Aura)
+          .reduce((acc, balance) => acc + Number(balance.balanceUsd), 0) || 0
+
+      const stakedBalancesAuraUsd =
+        pool.userBalance?.stakedBalances
+          .filter(balance => balance.stakingType === GqlPoolStakingType.Aura)
+          .reduce((acc, balance) => acc + Number(balance.balanceUsd), 0) || 0
+
       const walletBalanceUsd = pool.userBalance?.walletBalanceUsd || 0
 
-      if (stakedBalanceUsd > 0) {
+      if (stakedBalancesBalUsd > 0) {
         expandedPools.push({
           ...pool,
-          poolType: isVeBal ? ExpandedPoolType.Locked : ExpandedPoolType.Staked,
-          poolPositionUsd: stakedBalanceUsd,
+          poolType: isVeBal ? ExpandedPoolType.Locked : ExpandedPoolType.StakedBal,
+          poolPositionUsd: stakedBalancesBalUsd,
+        })
+      }
+
+      if (stakedBalancesAuraUsd > 0) {
+        expandedPools.push({
+          ...pool,
+          poolType: ExpandedPoolType.StakedAura,
+          poolPositionUsd: stakedBalancesAuraUsd,
         })
       }
 
@@ -45,7 +60,7 @@ export function useExpandedPools(pools: Pool[]) {
         })
       }
 
-      if (stakedBalanceUsd === 0 && walletBalanceUsd === 0) {
+      if (stakedBalancesBalUsd === 0 && stakedBalancesAuraUsd === 0 && walletBalanceUsd === 0) {
         expandedPools.push({
           ...pool,
           poolType: ExpandedPoolType.Default,
