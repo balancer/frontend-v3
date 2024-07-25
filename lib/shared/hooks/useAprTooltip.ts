bd9f0bbd8021311fe4c43785045ab9bc840ad6f5
Alberto Gualis <agualis@gmail.com>
2024-07-25 13:23:45 +0200

    chore: change surplus labels

--- lib/shared/hooks/useAprTooltip.ts
+++ lib/shared/hooks/useAprTooltip.ts
@@ -13,7 +13,7 @@ export const inherentTokenYieldTooltipText = `Inherent token yield,
 export const merklIncentivesTooltipText = `Merkl is a platform that allows 3rd party Incentive Providers
  to offer campaigns with additional yield for Liquidity Providers.`
 
-export const surplusIncentivesTooltipText = `Surplus incentives from CoW AMM pools.`
+export const surplusIncentivesTooltipText = `CoW AMM surplus`
 
 export const extraBalTooltipText = `veBAL holders can get an extra boost of up to 2.5x on their staking yield.
 The more veBAL held, the higher the boost.`
