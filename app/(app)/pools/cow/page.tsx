880efc521e91212e2031d88c00b893a9edc88890
Gareth Fuller <gareth.a.f@gmail.com>
2024-07-25 15:30:38 +0100

    feat: /pools/cow (#863)
    * feat: Add /pools/cow page
    
    * chore: Adjust metadata
    
    * chore: Use CoW theme for pools page
    
    * chore: Re-hide cow filter
    
    * chore: Update header
    
    * chore: Update header

---
+++ app/(app)/pools/cow/page.tsx
@@ -0,0 +1,34 @@
+import { PoolList } from '@/lib/modules/pool/PoolList/PoolList'
+import FadeInOnView from '@/lib/shared/components/containers/FadeInOnView'
+import { GqlPoolType } from '@/lib/shared/services/api/generated/graphql'
+import { Skeleton } from '@chakra-ui/react'
+import { Suspense } from 'react'
+import { Metadata } from 'next'
+import { CowFooter } from './CowFooter'
+import { DefaultPageContainer } from '@/lib/shared/components/containers/DefaultPageContainer'
+import { CowHeader } from './CowHeader'
+
+export const metadata: Metadata = {
+  title: `CoWAMM DeFi Liquidity Pools`,
+  description: `
+    CoW AMM protects LPs from LVR so they can provide liquidity
+    with less risk and more return.
+  `,
+}
+
+export default function PoolsPage() {
+  return (
+    <>
+      <DefaultPageContainer>
+        <CowHeader />
+        <FadeInOnView animateOnce={false}>
+          <Suspense fallback={<Skeleton w="full" h="500px" />}>
+            <PoolList fixedPoolTypes={[GqlPoolType.CowAmm]} />
+          </Suspense>
+        </FadeInOnView>
+      </DefaultPageContainer>
+
+      <CowFooter />
+    </>
+  )
+}
