8403eb1072bec0b55c8971c5904fab41f3b64f78
Anastasios <anastas3993@gmail.com>
2024-07-30 08:59:33 +0400

    fix: rewards update on claim

--- lib/modules/portfolio/PortfolioClaim/ClaimProtocolRevenueModal.tsx
+++ lib/modules/portfolio/PortfolioClaim/ClaimProtocolRevenueModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Modal, ModalBody, ModalCloseButton, ModalContent, VStack, Card } from '@chakra-ui/react'
-import { usePortfolio } from '@/lib/modules/portfolio/PortfolioProvider'
+import { UsePortfolio, usePortfolio } from '@/lib/modules/portfolio/PortfolioProvider'
 import { GqlChain } from '@/lib/shared/services/api/generated/graphql'
 import { Address } from 'viem'
 import { useBreakpoints } from '@/lib/shared/hooks/useBreakpoints'
@@ -17,6 +17,8 @@ import { HumanTokenAmountWithAddress } from '../../tokens/token.types'
 import { ActionModalFooter } from '@/lib/shared/components/modals/ActionModalFooter'
 import { SuccessOverlay } from '@/lib/shared/components/modals/SuccessOverlay'
 import { useClaimVeBalRewardsStep } from '../../pool/actions/claim/useClaimVeBalRewardsStep'
+import { useEffect, useState } from 'react'
+import BigNumber from 'bignumber.js'
 
 type Props = {
   isOpen: boolean
@@ -26,13 +28,29 @@ type Props = {
 export default function ClaimProtocolRevenueModal({ isOpen, onClose }: Props) {
   const { protocolRewardsData, protocolRewardsBalance, refetchProtocolRewards } = usePortfolio()
   const { isDesktop, isMobile } = useBreakpoints()
+  const [rewardsDataSnapshot, setRewardsDataSnapshot] = useState<
+    UsePortfolio['protocolRewardsData']
+  >([])
+  const [rewardsBalanceSnapshot, setRewardsBalanceSnapshot] = useState<BigNumber>(bn(0))
 
   const step = useClaimVeBalRewardsStep({ onSuccess: refetchProtocolRewards })
   const transactionSteps = useTransactionSteps([step])
 
+  useEffect(() => {
+    if (protocolRewardsData.length > 0 && rewardsDataSnapshot.length === 0) {
+      setRewardsDataSnapshot(protocolRewardsData)
+    }
+  }, [protocolRewardsData, rewardsDataSnapshot.length])
+
+  useEffect(() => {
+    if (protocolRewardsBalance.isGreaterThan(0) && rewardsBalanceSnapshot.isEqualTo(0)) {
+      setRewardsBalanceSnapshot(protocolRewardsBalance)
+    }
+  }, [protocolRewardsBalance, rewardsBalanceSnapshot])
+
   const claimTxHash = transactionSteps.lastTransaction?.result?.data?.transactionHash
 
-  const rewards: HumanTokenAmountWithAddress[] = protocolRewardsData
+  const rewards: HumanTokenAmountWithAddress[] = rewardsDataSnapshot
     .filter(reward => !bn(reward.balance).isZero())
     .map(reward => ({
       tokenAddress: reward.tokenAddress as Address,
@@ -63,7 +81,7 @@ export default function ClaimProtocolRevenueModal({ isOpen, onClose }: Props) {
               <TokenRowGroup
                 label={claimTxHash ? 'You got' : "You'll get"}
                 amounts={rewards}
-                totalUSDValue={protocolRewardsBalance.toString()}
+                totalUSDValue={rewardsBalanceSnapshot.toString()}
                 chain={GqlChain.Mainnet}
               />
             </Card>
