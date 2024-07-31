fdd230b65a5f442d1704ef95f7b66059fc4d4210
Alberto Gualis <alberto@balancer.finance>
2024-07-31 10:36:24 +0200

    feat: play moo error sound on CoW remove (#899)
    * feat: use error moo sound on CoW remove
    
    * refactor: function to const

--- lib/modules/transactions/transaction-steps/useSuccessSound.ts
+++
@@ -1,9 +0,0 @@
-import useSound from 'use-sound'
-
-export function useSuccessSound() {
-  const isCowTransaction = window.location.pathname.includes('cow')
-
-  const soundSrc = isCowTransaction ? '/sounds/moo.mp3' : '/sounds/gong.mp3'
-  const [playSuccessSound] = useSound(soundSrc)
-  return { playSuccessSound }
-}
