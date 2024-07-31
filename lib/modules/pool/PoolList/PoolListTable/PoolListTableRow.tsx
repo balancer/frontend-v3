8abf8b9295e4de1cdc968b7062beead543e54ad8
uiuxxx <uxuidefi@gmail.com>
2024-07-31 11:47:51 +0100

    Fade in animation of pool list rows with appropriate padding

--- lib/modules/pool/PoolList/PoolListTable/PoolListTableRow.tsx
+++ lib/modules/pool/PoolList/PoolListTable/PoolListTableRow.tsx
@@ -9,6 +9,7 @@ import { usePoolListQueryState } from '../usePoolListQueryState'
 import { PoolListItem } from '../../pool.types'
 import { PoolListTokenPills } from '../PoolListTokenPills'
 import { getUserTotalBalanceUsd } from '../../user-balance.helpers'
+import FadeInOnView from '@/lib/shared/components/containers/FadeInOnView'
 
 interface Props extends GridProps {
   pool: PoolListItem
@@ -22,70 +23,72 @@ export function PoolListTableRow({ pool, keyValue, ...rest }: Props) {
   const { toCurrency } = useCurrency()
 
   return (
-    <Box
-      key={keyValue}
-      transition="all 0.2s ease-in-out"
-      _hover={{
-        bg: 'background.level0',
-      }}
-      rounded="md"
-      px={{ base: '0', sm: 'md' }}
-      w="full"
-    >
-      <Link href={getPoolPath(pool)} prefetch={true}>
-        <Grid {...rest} py="sm" pr="4">
-          <GridItem>
-            <NetworkIcon chain={pool.chain} size={6} />
-          </GridItem>
-          <GridItem>
-            <PoolListTokenPills
-              pool={pool}
-              h={['32px', '36px']}
-              p={['xxs', 'sm']}
-              pr={[1.5, 'ms']}
-              iconSize={20}
-            />
-          </GridItem>
-          <GridItem>
-            <Text textAlign="left" fontWeight="medium" textTransform="capitalize">
-              {getPoolTypeLabel(pool.type)}
-            </Text>
-          </GridItem>
-          {userAddress && (
+    <FadeInOnView animateOnce={false}>
+      <Box
+        key={keyValue}
+        transition="all 0.2s ease-in-out"
+        _hover={{
+          bg: 'background.level0',
+        }}
+        rounded="md"
+        px={{ base: '0', sm: 'md' }}
+        w="full"
+      >
+        <Link href={getPoolPath(pool)} prefetch={true}>
+          <Grid {...rest} py={{ base: 'ms', md: 'md' }} pr="4">
             <GridItem>
-              <Text textAlign="right" fontWeight="medium">
-                {toCurrency(getUserTotalBalanceUsd(pool), { abbreviated: false })}
+              <NetworkIcon chain={pool.chain} size={6} />
+            </GridItem>
+            <GridItem>
+              <PoolListTokenPills
+                pool={pool}
+                h={['32px', '36px']}
+                p={['xxs', 'sm']}
+                pr={[1.5, 'ms']}
+                iconSize={20}
+              />
+            </GridItem>
+            <GridItem>
+              <Text textAlign="left" fontWeight="medium" textTransform="capitalize">
+                {getPoolTypeLabel(pool.type)}
               </Text>
             </GridItem>
-          )}
-          <GridItem>
-            <Text
-              title={toCurrency(pool.dynamicData.totalLiquidity, { abbreviated: false })}
-              textAlign="right"
-              fontWeight="medium"
-            >
-              {toCurrency(pool.dynamicData.totalLiquidity)}
-            </Text>
-          </GridItem>
-          <GridItem textAlign="right">
-            <Text
-              title={toCurrency(pool.dynamicData.volume24h, { abbreviated: false })}
-              textAlign="right"
-              fontWeight="medium"
-            >
-              {toCurrency(pool.dynamicData.volume24h)}
-            </Text>
-          </GridItem>
-          <GridItem justifySelf="end" pr={{ base: 'md', xl: '0' }}>
-            <MemoizedMainAprTooltip
-              aprItems={pool.dynamicData.aprItems}
-              poolId={pool.id}
-              textProps={{ fontWeight: 'medium' }}
-              pool={pool}
-            />
-          </GridItem>
-        </Grid>
-      </Link>
-    </Box>
+            {userAddress && (
+              <GridItem>
+                <Text textAlign="right" fontWeight="medium">
+                  {toCurrency(getUserTotalBalanceUsd(pool), { abbreviated: false })}
+                </Text>
+              </GridItem>
+            )}
+            <GridItem>
+              <Text
+                title={toCurrency(pool.dynamicData.totalLiquidity, { abbreviated: false })}
+                textAlign="right"
+                fontWeight="medium"
+              >
+                {toCurrency(pool.dynamicData.totalLiquidity)}
+              </Text>
+            </GridItem>
+            <GridItem textAlign="right">
+              <Text
+                title={toCurrency(pool.dynamicData.volume24h, { abbreviated: false })}
+                textAlign="right"
+                fontWeight="medium"
+              >
+                {toCurrency(pool.dynamicData.volume24h)}
+              </Text>
+            </GridItem>
+            <GridItem justifySelf="end" pr={{ base: 'md', xl: '0' }}>
+              <MemoizedMainAprTooltip
+                aprItems={pool.dynamicData.aprItems}
+                poolId={pool.id}
+                textProps={{ fontWeight: 'medium' }}
+                pool={pool}
+              />
+            </GridItem>
+          </Grid>
+        </Link>
+      </Box>
+    </FadeInOnView>
   )
 }
