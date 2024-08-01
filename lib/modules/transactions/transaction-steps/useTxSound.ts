39cf8ca7607bf76e29ddab800737839256a787b5
Alberto Gualis <alberto@balancer.finance>
2024-08-01 17:17:13 +0200

    fix: usePathName instead of window to detect CoW urls (#908)
    * fix: usePathName instead of window to detect CoW urls
    
    * fix: usePathname in tests

--- lib/modules/transactions/transaction-steps/useTxSound.ts
+++ lib/modules/transactions/transaction-steps/useTxSound.ts
@@ -1,8 +1,11 @@
 import useSound from 'use-sound'
 import { StepType } from './lib'
+import { usePathname } from 'next/navigation'
 
 export function useTxSound() {
-  const isCowTransaction = window.location.pathname.includes('cow')
+  const pathname = usePathname()
+
+  const isCowTransaction = pathname.includes('/cow/')
   const successSoundSrc = isCowTransaction ? '/sounds/successMoo.mp3' : '/sounds/gong.mp3'
   const [playSuccessSound] = useSound(successSoundSrc)
 
