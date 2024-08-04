bd064e49a107d3fd64481d80e839021623ca793e
Anastasios <anastas3993@gmail.com>
2024-08-04 21:59:59 +0400

    feat: add lock actions logic

---
+++ lib/modules/vebal/lock/lockTransactionHelpers.ts
@@ -0,0 +1,91 @@
+import { toUtcTime } from '@/lib/shared/utils/time'
+import { RawAmount } from '../../tokens/approvals/approval-rules'
+
+export enum LockActionType {
+  CreateLock = 'create_lock',
+  IncreaseLock = 'increase_amount',
+  ExtendLock = 'increase_unlock_time',
+  Unlock = 'withdraw',
+}
+export function parseDate(date: string) {
+  return (toUtcTime(new Date(date)) / 1000).toString()
+}
+
+export function getInitLabel(lockActionType: LockActionType) {
+  switch (lockActionType) {
+    case LockActionType.CreateLock:
+      return 'Create Lock'
+    case LockActionType.ExtendLock:
+      return 'Extend Lock'
+    case LockActionType.IncreaseLock:
+      return 'Increase Lock'
+    case LockActionType.Unlock:
+      return 'Unlock'
+    default:
+      return ''
+  }
+}
+
+export function getDescription(lockActionType: LockActionType) {
+  switch (lockActionType) {
+    case LockActionType.CreateLock:
+      return 'Create a new lock for the specified amount and end date.'
+    case LockActionType.ExtendLock:
+      return 'Extend an existing lock.'
+    case LockActionType.IncreaseLock:
+      return 'Increase the amount of an existing lock.'
+    case LockActionType.Unlock:
+      return 'Unlock'
+    default:
+      return ''
+  }
+}
+
+export function getConfirmingLabel(lockActionType: LockActionType) {
+  switch (lockActionType) {
+    case LockActionType.CreateLock:
+      return 'Confirming lock creation...'
+    case LockActionType.ExtendLock:
+      return 'Confirming lock extension...'
+    case LockActionType.IncreaseLock:
+      return 'Confirming lock increase...'
+    case LockActionType.Unlock:
+      return 'Confirming unlock...'
+    default:
+      return ''
+  }
+}
+
+export function getConfirmedLabel(
+  lockActionType: LockActionType,
+  lockAmount: RawAmount,
+  lockEndDate: string
+) {
+  switch (lockActionType) {
+    case LockActionType.CreateLock:
+      return `Lock created for ${lockAmount.rawAmount} tokens until ${lockEndDate}`
+    case LockActionType.ExtendLock:
+      return `Lock extended until ${lockEndDate}`
+    case LockActionType.IncreaseLock:
+      return `Lock amount increased by ${lockAmount.rawAmount}`
+    case LockActionType.Unlock:
+      return 'Lock unlocked'
+    default:
+      return ''
+  }
+}
+
+export function getTooltip(lockActionType: LockActionType) {
+  switch (lockActionType) {
+    case LockActionType.CreateLock:
+      return 'Create a new lock for the specified amount and end date.'
+    case LockActionType.ExtendLock:
+      return 'Extend the end date of an existing lock.'
+    case LockActionType.IncreaseLock:
+      return 'Increase the amount of an existing lock.'
+    case LockActionType.Unlock:
+      return 'Unlock the specified lock.'
+    default:
+      return ''
+  }
+}
---
+++ lib/modules/vebal/lock/useLockSteps.tsx
@@ -0,0 +1,115 @@
+import { useMemo, useCallback } from 'react'
+import { useTokenApprovalSteps } from '../../tokens/approvals/useTokenApprovalSteps'
+import { useUserAccount } from '../../web3/UserAccountProvider'
+import mainnetNetworkConfig from '@/lib/config/networks/mainnet'
+import { RawAmount } from '../../tokens/approvals/approval-rules'
+import { ManagedTransactionButton } from '@/lib/modules/transactions/transaction-steps/TransactionButton'
+import {
+  TransactionLabels,
+  TransactionStep,
+} from '@/lib/modules/transactions/transaction-steps/lib'
+import { sentryMetaForWagmiSimulation } from '@/lib/shared/utils/query-errors'
+import { parseUnits } from 'viem'
+import { ManagedTransactionInput } from '../../web3/contracts/useManagedTransaction'
+import {
+  LockActionType,
+  getDescription,
+  getConfirmingLabel,
+  getConfirmedLabel,
+  getInitLabel,
+  getTooltip,
+  parseDate,
+} from './lockTransactionHelpers'
+
+type UseCreateLockArgs = {
+  lockAmount: RawAmount
+  lockEndDate: string
+  lockActionType: LockActionType
+}
+
+export function useLockSteps({ lockAmount, lockEndDate, lockActionType }: UseCreateLockArgs) {
+  const { userAddress } = useUserAccount()
+  const amount = lockAmount.rawAmount.toString()
+  const { isLoading: isLoadingTokenApprovalSteps, steps: tokenApprovalSteps } =
+    useTokenApprovalSteps({
+      spenderAddress: mainnetNetworkConfig.contracts.veBAL,
+      chain: mainnetNetworkConfig.chain,
+      approvalAmounts: [lockAmount],
+      actionType: 'Locking',
+    })
+
+  const labels: TransactionLabels = useMemo(
+    () => ({
+      init: getInitLabel(lockActionType),
+      title: getInitLabel(lockActionType),
+      description: getDescription(lockActionType),
+      confirming: getConfirmingLabel(lockActionType),
+      confirmed: getConfirmedLabel(lockActionType, lockAmount, lockEndDate),
+      tooltip: getTooltip(lockActionType),
+    }),
+    [lockActionType, lockAmount, lockEndDate]
+  )
+
+  const txSimulationMeta = sentryMetaForWagmiSimulation(
+    'Error in wagmi tx simulation (Lock transaction)',
+    {
+      userAddress,
+      lockAmount: lockAmount.rawAmount.toString(),
+      lockEndDate,
+      lockActionType,
+    }
+  )
+
+  const props: ManagedTransactionInput = useMemo(() => {
+    function getArgs() {
+      switch (lockActionType) {
+        case LockActionType.CreateLock:
+          return [parseUnits(amount, 18), parseDate(lockEndDate)]
+        case LockActionType.ExtendLock:
+          return [parseDate(lockEndDate)]
+        case LockActionType.IncreaseLock:
+          return [parseUnits(amount, 18)]
+        default:
+          return []
+      }
+    }
+
+    return {
+      enabled: !!lockAmount.rawAmount && !!lockEndDate,
+      labels,
+      chainId: mainnetNetworkConfig.chainId,
+      contractId: 'balancer.veBAL',
+      contractAddress: mainnetNetworkConfig.contracts.veBAL,
+      functionName: lockActionType,
+      args: getArgs() as any,
+      txSimulationMeta,
+    }
+  }, [lockAmount, lockEndDate, lockActionType, labels, txSimulationMeta, amount])
+
+  const onSuccess = useCallback(() => {
+    // Handle success actions
+  }, [])
+
+  const lockStep: TransactionStep = useMemo(
+    () => ({
+      id: lockActionType,
+      stepType: lockActionType,
+      labels,
+      contractId: 'veBAL',
+      contractAddress: mainnetNetworkConfig.contracts.veBAL,
+      isComplete: () => false,
+      // onActivated: () => {},
+      // onDeactivated: () => {},
+      onSuccess,
+      renderAction: () => <ManagedTransactionButton id={lockActionType.toString()} {...props} />,
+    }),
+    [labels, onSuccess, props, lockActionType]
+  )
+
+  const steps = [lockStep, ...tokenApprovalSteps]
+
+  return {
+    isLoadingSteps: isLoadingTokenApprovalSteps,
+    steps,
+  }
+}
