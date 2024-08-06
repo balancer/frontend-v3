b53989bc2e249b77129a94e7255c13715547145e
uiuxxx <uxuidefi@gmail.com>
2024-08-06 13:09:04 +0200

    New CoW promo banner and removal of featured pools

--- app/(app)/pools/page.tsx
+++ app/(app)/pools/page.tsx
@@ -1,33 +1,17 @@
-import { getProjectConfig } from '@/lib/config/getProjectConfig'
-import { FeaturedPools } from '@/lib/modules/featured-pools/FeaturedPools'
 import { PoolList } from '@/lib/modules/pool/PoolList/PoolList'
 import { DefaultPageContainer } from '@/lib/shared/components/containers/DefaultPageContainer'
 import FadeInOnView from '@/lib/shared/components/containers/FadeInOnView'
-import { getApolloServerClient } from '@/lib/shared/services/api/apollo-server.client'
-import { GetFeaturedPoolsDocument } from '@/lib/shared/services/api/generated/graphql'
+
 import { Box, Skeleton } from '@chakra-ui/react'
 import { Suspense } from 'react'
+import { CowPromolBanner } from './cow/CowPromoBanner'
 
 export default async function PoolsPage() {
-  const { supportedNetworks } = getProjectConfig()
-
-  const featuredPoolsQuery = await getApolloServerClient().query({
-    query: GetFeaturedPoolsDocument,
-    variables: { chains: supportedNetworks },
-    context: {
-      fetchOptions: {
-        next: { revalidate: 300 }, // 5 minutes
-      },
-    },
-  })
-
-  const featuredPools = featuredPoolsQuery.data.featuredPools || []
-
   return (
     <DefaultPageContainer>
       <FadeInOnView animateOnce={false}>
         <Box mb={{ base: '2xl', sm: '3xl' }}>
-          <FeaturedPools featuredPools={featuredPools} />
+          <CowPromolBanner />
         </Box>
       </FadeInOnView>
       <FadeInOnView animateOnce={false}>
