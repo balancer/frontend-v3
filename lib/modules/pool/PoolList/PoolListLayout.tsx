b24423c5cf625853daf2f09b0dc7ea72fbb46de7
uiuxxx <uxuidefi@gmail.com>
2024-08-07 14:57:41 +0200

    Remove vertical filter jank from filter tags and reset button

--- lib/modules/pool/PoolList/PoolListLayout.tsx
+++ lib/modules/pool/PoolList/PoolListLayout.tsx
@@ -1,13 +1,25 @@
 'use client'
 
-import { Heading, Stack, HStack, VStack } from '@chakra-ui/react'
-import { FilterTags, PoolListFilters } from './PoolListFilters'
+import { Box, Heading, Stack, HStack, VStack, useBreakpointValue } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import { FilterTags, PoolListFilters, useFilterTagsVisible } from './PoolListFilters'
 import { PoolListTable } from './PoolListTable/PoolListTable'
 import { usePoolList } from './PoolListProvider'
 import { fNum } from '@/lib/shared/utils/numbers'
 
 export function PoolListLayout() {
   const { pools, loading, count } = usePoolList()
+  const isFilterVisible = useFilterTagsVisible()
+  const isMd = useBreakpointValue({ base: false, md: true })
+
+  const variants = {
+    visible: {
+      transform: isMd ? 'translateY(-40px)' : 'translateY(0)',
+    },
+    hidden: {
+      transform: 'translateY(0)',
+    },
+  }
 
   return (
     <VStack align="start" spacing="md" w="full">
@@ -15,16 +27,32 @@ export function PoolListLayout() {
         direction={{ base: 'column', md: 'row' }}
         w="full"
         justify="space-between"
-        align="start"
+        align={isFilterVisible ? 'end' : 'start'}
       >
-        <VStack align="start" w="full">
+        <VStack align="start" w="full" pb={{ base: 'sm', md: '0' }}>
           <HStack w="full">
-            <Heading as="h2" size="lg" variant="special">
-              Liquidity pools
-            </Heading>
-            <Heading size="md" variant="secondary" mt="1">
-              ({fNum('integer', count || 0)})
-            </Heading>
+            <Box position="relative" top="0">
+              <Box
+                as={motion.div}
+                position={{ base: 'relative', md: 'absolute' }}
+                top="0"
+                left="0"
+                transition="all 0.15s var(--ease-out-cubic)"
+                minW={{ base: 'auto', md: '270px' }}
+                willChange="transform"
+                variants={variants}
+                animate={isFilterVisible ? 'visible' : 'hidden'}
+              >
+                <HStack w="full">
+                  <Heading as="h2" size="lg" variant="special">
+                    Liquidity pools
+                  </Heading>
+                  <Heading size="md" variant="secondary" mt="1">
+                    ({fNum('integer', count || 0)})
+                  </Heading>
+                </HStack>
+              </Box>
+            </Box>
           </HStack>
           <FilterTags />
         </VStack>
