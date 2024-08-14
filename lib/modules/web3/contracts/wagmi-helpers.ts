8b4f9c1c7ed01e1b123ec54da44c9d6fe4847b5d
Alberto Gualis <alberto@balancer.finance>
2024-08-14 15:58:30 +0200

    fix: add explicit waitForTransactionReceipt timeouts (#962)
    * fix: add explicit waitForTransactionReceipt timeouts
    
    * fix: add placeholderData option

--- lib/modules/web3/contracts/wagmi-helpers.ts
+++ lib/modules/web3/contracts/wagmi-helpers.ts
@@ -1,6 +1,8 @@
 // Helpers for wagmi transactions
 import { Address } from 'viem'
 import { TransactionBundle } from './contract.types'
+import { polygon } from 'viem/chains'
+import { secs } from '@/lib/shared/utils/time'
 
 export function getHashFromTransaction(transactionBundle?: TransactionBundle): Address | undefined {
   if (!transactionBundle) return
@@ -15,4 +17,17 @@ export function isValidUserAddress(userAddress?: Address) {
   if (!userAddress) return false
   if (userAddress === noUserAddress) return false
   return true
+}
+
+/*
+  Returns timeout to be used in useWaitForTransactionReceipt options
+  By default all react queries retry 3 times
+  More info: https://tanstack.com/query/v5/docs/framework/react/guides/query-retries
+ */
+export function getWaitForReceiptTimeout(chainId: number) {
+  // In polygon there will be 3 retries of 25 seconds until we throw the timeout error
+  if (chainId === polygon.id) return secs(25).toMs()
+
+  // In other chains there will be 3 retries of 15 seconds until we throw the timeout error
+  return secs(15).toMs()
 }
