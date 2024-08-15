4eef0f3fdfa72aeed09825cf712497829c8759b7
groninge <groninge@gmail.com>
2024-08-15 11:22:21 +0200

    grouped routes

--- app/(beets)/sftmx/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { DefaultPageContainer } from '@/lib/shared/components/containers/DefaultPageContainer'
-import { PropsWithChildren } from 'react'
-
-export default async function SftmxLayout({ children }: PropsWithChildren) {
-  return <DefaultPageContainer minH="100vh">{children}</DefaultPageContainer>
-}
