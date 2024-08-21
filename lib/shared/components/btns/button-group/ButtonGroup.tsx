4bb114168e1d94ae9fc0257955021b7137c5c798
groninge <groninge@gmail.com>
2024-08-21 07:55:03 +0200

    remove size & weight

--- lib/shared/components/btns/button-group/ButtonGroup.tsx
+++ lib/shared/components/btns/button-group/ButtonGroup.tsx
@@ -54,12 +54,7 @@ export default function ButtonGroup({
                   inset="0"
                 ></Box>
               )}
-              <Box
-                position="relative"
-                zIndex="8"
-                fontSize={{ base: 'xs', md: 'md' }}
-                fontWeight="500"
-              >
+              <Box position="relative" zIndex="8">
                 {option.label}
               </Box>
             </Button>
