1458570a896b141c7fd11f8f43dfc25c53d99e26
uiuxxx <uxuidefi@gmail.com>
2024-08-22 23:53:21 +0100

    Update sortable icon

--- lib/shared/components/icons/SortableIcon.tsx
+++ lib/shared/components/icons/SortableIcon.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
 export function SortableIcon() {
   return (
-    <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        d="m2 6 2.667-2.666m0 0 2.666 2.667M4.667 3.334v9.333M14 10.001l-2.666 2.666m0 0-2.667-2.666m2.667 2.666V3.334"
+        d="m1 6 2.667-2.666m0 0 2.666 2.667M3.667 3.334v9.333M13 10.001l-2.666 2.666m0 0-2.667-2.666m2.667 2.666V3.334"
         stroke="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
