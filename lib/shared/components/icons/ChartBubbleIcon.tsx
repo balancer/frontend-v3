77cecde59283083507bf23cde6e3c6025eaa0c6b
groninge <groninge@gmail.com>
2024-08-23 09:08:08 +0200

    fix icon props

--- lib/shared/components/icons/ChartBubbleIcon.tsx
+++ lib/shared/components/icons/ChartBubbleIcon.tsx
@@ -12,9 +12,9 @@ export function ChartBubbleIcon({ size = 24 }: { size?: number }) {
         <path
           d="M1.555 13.611a2.75 2.75 0 1 1 3.89-3.889 2.75 2.75 0 0 1-3.89 3.89Zm10.041 1.126a1.75 1.75 0 1 1 2.475-2.475 1.75 1.75 0 0 1-2.475 2.475ZM8.848 7.152a3.75 3.75 0 1 1 5.304-5.304 3.75 3.75 0 0 1-5.304 5.304Z"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
