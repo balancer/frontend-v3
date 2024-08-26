ff23b67d2845662832c92df98420f0f5dec2a652
groninge <groninge@gmail.com>
2024-08-26 11:42:34 +0200

    use dynamic

--- lib/shared/components/navs/Footer.tsx
+++ lib/shared/components/navs/Footer.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { getProjectConfig } from '@/lib/config/getProjectConfig'
-import { lazily } from 'react-lazily'
+import dynamic from 'next/dynamic'
 
 export function Footer() {
   const { projectId } = getProjectConfig()
-  const { FooterContent } = lazily(() => import(`./Footer.${projectId}`))
+  const FooterContent = dynamic(() =>
+    import(`./Footer.${projectId}`).then(mod => mod.FooterContent)
+  )
 
   return <FooterContent />
 }
