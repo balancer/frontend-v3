a93dc43cdab864e37c714e3751cbe693d3a897f7
Alberto Gualis <agualis@gmail.com>
2024-09-09 13:30:32 +0200

    fix: integration tests for nested pool

--- lib/modules/pool/actions/add-liquidity/handlers/NestedAddLiquidity.handler.integration.spec.ts
+++ lib/modules/pool/actions/add-liquidity/handlers/NestedAddLiquidity.handler.integration.spec.ts
@@ -2,20 +2,25 @@
 import networkConfig from '@/lib/config/networks/mainnet'
 import { daiAddress, usdcAddress, usdtAddress, wETHAddress } from '@/lib/debug-helpers'
 import { mainnetTestPublicClient } from '@/test/utils/wagmi/wagmi-test-clients'
-import { nestedPoolMock } from '../../../__mocks__/nestedPoolMock'
 import { Pool } from '../../../PoolProvider'
 import { NestedAddLiquidityHandler } from './NestedAddLiquidity.handler'
 import { selectAddLiquidityHandler } from './selectAddLiquidityHandler'
 import { defaultTestUserAccount } from '@/test/anvil/anvil-setup'
 import { HumanTokenAmountWithAddress } from '@/lib/modules/tokens/token.types'
+import { getPoolMock } from '../../../__mocks__/getPoolMock'
+import { GqlChain } from '@/lib/shared/services/api/generated/graphql'
 
 function selectNestedHandler(pool: Pool) {
   return selectAddLiquidityHandler(pool) as NestedAddLiquidityHandler
 }
 
+// Balancer 50WETH-50-3pool
+const poolId = '0x08775ccb6674d6bdceb0797c364c2653ed84f3840002000000000000000004f0'
+const nestedPool = await getPoolMock(poolId, GqlChain.Mainnet)
+
 describe('When adding nested liquidity for a weighted pool', () => {
   test('has zero price impact', async () => {
-    const handler = selectNestedHandler(nestedPoolMock)
+    const handler = selectNestedHandler(nestedPool)
 
     const humanAmountsIn: HumanTokenAmountWithAddress[] = [
       { humanAmount: '100', tokenAddress: daiAddress },
@@ -25,7 +30,7 @@ describe('When adding nested liquidity for a weighted pool', () => {
   })
 
   test('with single token input', async () => {
-    const handler = selectNestedHandler(nestedPoolMock)
+    const handler = selectNestedHandler(nestedPool)
 
     const humanAmountsIn: HumanTokenAmountWithAddress[] = [
       { humanAmount: '1', tokenAddress: daiAddress },
@@ -37,7 +42,7 @@ describe('When adding nested liquidity for a weighted pool', () => {
   })
 
   test('with multiple token input', async () => {
-    const handler = selectNestedHandler(nestedPoolMock)
+    const handler = selectNestedHandler(nestedPool)
 
     const humanAmountsIn: HumanTokenAmountWithAddress[] = [
       { humanAmount: '1', tokenAddress: wETHAddress },
@@ -56,7 +61,7 @@ describe('When adding nested liquidity for a weighted pool', () => {
       { humanAmount: '1', tokenAddress: daiAddress },
     ]
 
-    const handler = selectNestedHandler(nestedPoolMock)
+    const handler = selectNestedHandler(nestedPool)
 
     // Store query response in handler instance
     const queryOutput = await handler.simulate(humanAmountsIn)
