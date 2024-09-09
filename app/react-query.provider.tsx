5f48f65d250c7580a0b0acccb553e57da089594d
Alberto Gualis <agualis@gmail.com>
2024-09-09 12:08:15 +0200

    fix: sentry stack trace detection

--- app/react-query.provider.tsx
+++ app/react-query.provider.tsx
@@ -1,11 +1,7 @@
 'use client'
 
 import { captureError } from '@/lib/shared/utils/errors'
-import {
-  SentryMetadata,
-  captureSentryError,
-  shouldIgnoreError,
-} from '@/lib/shared/utils/query-errors'
+import { SentryMetadata, captureSentryError } from '@/lib/shared/utils/query-errors'
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
 
@@ -13,7 +9,6 @@ export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     // Global handler for every react-query error
     onError: (error, query) => {
-      if (shouldIgnoreError(error)) return
       console.log('Sentry capturing error: ', {
         meta: query?.meta,
         error,
