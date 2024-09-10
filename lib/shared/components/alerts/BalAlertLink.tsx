171631c2681f6ae170ebde8c4014e792fc4fa49f
Alberto Gualis <agualis@gmail.com>
2024-09-10 16:58:59 +0200

    chore: add underline text decoration to link

--- lib/shared/components/alerts/BalAlertLink.tsx
+++ lib/shared/components/alerts/BalAlertLink.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react'
 
 export function BalAlertLink({ href, children, ...rest }: PropsWithChildren<LinkProps>) {
   return (
-    <Link href={href} variant="nav" color="white" target="_blank" {...rest}>
+    <Link href={href} color="font.dark" textDecoration="underline" target="_blank" {...rest}>
       {children}
     </Link>
   )
