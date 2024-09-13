c04ef53863fbe6c209c22ddb0b9e1641b7dfc5c5
Alberto Gualis <alberto@balancer.finance>
2024-09-13 14:56:44 +0200

    fix: avoid unwanted background simulation refetches (#1082)
    * fix: background simulation refetches
    
    * fix: avoid background refetches for claimable and rewards queries
    
    * refactor: use onlyExplicitRefetch instead of staleTime

--- lib/modules/web3/contracts/useManagedTransaction.ts
+++ lib/modules/web3/contracts/useManagedTransaction.ts
@@ -15,6 +15,7 @@ import { useOnTransactionSubmission } from './useOnTransactionSubmission'
 import { captureWagmiExecutionError } from '@/lib/shared/utils/query-errors'
 import { useTxHash } from '../safe.hooks'
 import { getWaitForReceiptTimeout } from './wagmi-helpers'
+import { onlyExplicitRefetch } from '@/lib/shared/utils/queries'
 
 type IAbiMap = typeof AbiMap
 type AbiMapKey = keyof typeof AbiMap
@@ -53,6 +54,8 @@ export function useManagedTransaction({
     query: {
       enabled: enabled && !shouldChangeNetwork,
       meta: txSimulationMeta,
+      // In chains like polygon, we don't want background refetches while waiting for min block confirmations
+      ...onlyExplicitRefetch,
     },
   })
 
