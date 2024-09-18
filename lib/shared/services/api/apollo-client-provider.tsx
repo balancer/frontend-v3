579fa8b762d55265164091bd1422d9030c7c5741
Gareth Fuller <gareth.a.f@gmail.com>
2024-09-18 10:16:46 +0100

    chore: Update deps for monorepo (#1101)
    * chore: Update deps
    
    * chore: Remove comment

--- lib/shared/services/api/apollo-client-provider.tsx
+++ lib/shared/services/api/apollo-client-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // eslint-disable-next-line max-len
-import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support/ssr'
+import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support'
 import { createApolloClient } from '@/lib/shared/services/api/apollo.client'
 
 export function ApolloClientProvider({ children }: React.PropsWithChildren) {
