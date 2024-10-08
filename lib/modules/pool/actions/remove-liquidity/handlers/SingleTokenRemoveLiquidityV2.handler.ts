e48e6b413d261c9d492c0ef677fe56f5ae83a6c3
Alberto Gualis <agualis@gmail.com>
2024-10-08 10:55:52 +0200

    chore: clean v2 single token remove handler

--- lib/modules/pool/actions/remove-liquidity/handlers/SingleTokenRemoveLiquidityV2.handler.ts
+++ lib/modules/pool/actions/remove-liquidity/handlers/SingleTokenRemoveLiquidityV2.handler.ts
@@ -1,74 +1,10 @@
 import { TransactionConfig } from '@/lib/modules/web3/contracts/contract.types'
-import { getRpcUrl } from '@/lib/modules/web3/transports'
-import { SentryError } from '@/lib/shared/utils/errors'
-import {
-  HumanAmount,
-  InputAmount,
-  PriceImpact,
-  PriceImpactAmount,
-  RemoveLiquidity,
-  RemoveLiquidityKind,
-  RemoveLiquiditySingleTokenExactInInput,
-  Slippage,
-} from '@balancer/sdk'
-import { Address, parseEther } from 'viem'
-import { Pool } from '../../../PoolProvider'
-import { BPT_DECIMALS } from '../../../pool.constants'
-import { isV3Pool } from '../../../pool.helpers'
-import {
-  LiquidityActionHelpers,
-  formatBuildCallParams,
-  isEmptyHumanAmount,
-} from '../../LiquidityActionHelpers'
-import {
-  QueryRemoveLiquidityInput,
-  SdkBuildRemoveLiquidityInput,
-  SdkQueryRemoveLiquidityOutput,
-} from '../remove-liquidity.types'
-import { RemoveLiquidityHandler } from './RemoveLiquidity.handler'
+import { RemoveLiquidity, Slippage } from '@balancer/sdk'
+import { formatBuildCallParams } from '../../LiquidityActionHelpers'
+import { SdkBuildRemoveLiquidityInput } from '../remove-liquidity.types'
+import { BaseSingleTokenRemoveLiquidityHandler } from './BaseSingleTokenRemoveLiquidity.handler'
 
-export class SingleTokenRemoveLiquidityV2Handler implements RemoveLiquidityHandler {
-  helpers: LiquidityActionHelpers
-
-  constructor(pool: Pool) {
-    this.helpers = new LiquidityActionHelpers(pool)
-  }
-
-  public async simulate({
-    humanBptIn,
-    tokenOut,
-  }: QueryRemoveLiquidityInput): Promise<SdkQueryRemoveLiquidityOutput> {
-    const removeLiquidity = new RemoveLiquidity()
-    const removeLiquidityInput = this.constructSdkInput(humanBptIn, tokenOut)
-
-    const sdkQueryOutput = await removeLiquidity.query(removeLiquidityInput, this.helpers.poolState)
-
-    return { amountsOut: sdkQueryOutput.amountsOut, sdkQueryOutput }
-  }
-
-  public async getPriceImpact({
-    humanBptIn,
-    tokenOut,
-  }: QueryRemoveLiquidityInput): Promise<number> {
-    if (!tokenOut) {
-      throw new SentryError('TokenOut should never be undefined in Single Token remove liquidity')
-    }
-
-    if (isEmptyHumanAmount(humanBptIn) || !tokenOut) {
-      // Avoid price impact calculation
-      return 0
-    }
-
-    const removeLiquidityInput = this.constructSdkInput(humanBptIn, tokenOut)
-
-    const priceImpactABA: PriceImpactAmount = await PriceImpact.removeLiquidity(
-      removeLiquidityInput,
-      this.helpers.poolState
-    )
-
-    return priceImpactABA.decimal
-  }
-
+export class SingleTokenRemoveLiquidityV2Handler extends BaseSingleTokenRemoveLiquidityHandler {
   public async buildCallData({
     account,
     slippagePercent,
@@ -83,9 +19,7 @@ export class SingleTokenRemoveLiquidityV2Handler implements RemoveLiquidityHandl
       wethIsEth,
     }
 
-    const buildCallParams = isV3Pool(this.helpers.pool)
-      ? baseBuildCallParams
-      : formatBuildCallParams(baseBuildCallParams, account)
+    const buildCallParams = formatBuildCallParams(baseBuildCallParams, account)
 
     const { callData, to, value } = removeLiquidity.buildCall(buildCallParams)
 
@@ -95,28 +29,6 @@ export class SingleTokenRemoveLiquidityV2Handler implements RemoveLiquidityHandl
       data: callData,
       to,
       value,
-    }
-  }
-
-  /**
-   * PRIVATE METHODS
-   */
-  private constructSdkInput(
-    humanBptIn: HumanAmount,
-    tokenOut: Address
-  ): RemoveLiquiditySingleTokenExactInInput {
-    const bptInInputAmount: InputAmount = {
-      rawAmount: parseEther(humanBptIn),
-      decimals: BPT_DECIMALS,
-      address: this.helpers.pool.address as Address,
-    }
-
-    return {
-      chainId: this.helpers.chainId,
-      rpcUrl: getRpcUrl(this.helpers.chainId),
-      bptIn: bptInInputAmount,
-      kind: RemoveLiquidityKind.SingleTokenExactIn,
-      tokenOut,
     }
   }
 }
